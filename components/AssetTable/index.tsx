a3fae98491734091c88e1afcb722d8f9ec154ff1
Juani Gallo <ji.gallo93@gmail.com>
2022-01-04 16:21:03 -0300

    Finishes AssetModal component

--- components/AssetTable/index.tsx
+++ components/AssetTable/index.tsx
@@ -1,3 +1,4 @@
+import Button from 'components/common/Button';
 import Loading from 'components/common/Loading';
 import { Maturity } from 'types/Maturity';
 import styles from './style.module.scss';
@@ -12,22 +13,39 @@ function AssetTable({ maturities }: Props) {
       <div className={styles.row}>
         <div className={styles.maturity}>Maturity</div>
         <div className={styles.lastFixedRate}>Last fixed rate</div>
+        <div className={styles.actions}></div>
+
       </div>
       {maturities ? (
         <>
           {maturities.map((maturity: Maturity, key: number) => {
             return (
-              <div className={styles.row}>
+              <div className={styles.row} key={key}>
                 <div className={styles.maturity}>
                   <span>{maturity.label}</span>
                   <span className={styles.liquidity}>Liquidity: </span>
                 </div>
+                <div className={styles.lastFixedRate}>
+                  <div className={styles.deposit}>4,41%</div>
+                  <div className={styles.borrow}>3,16%</div>
+                </div>
+                <div className={styles.actions}>
+                  <div className={styles.buttonContainer}>
+                    <Button text="Deposit" className="primary" />
+                  </div>
+                  <div className={styles.buttonContainer}>
+                    <Button text="Borrow" className="secondary" />
+                  </div>
+                </div>
+
               </div>
             );
           })}
         </>
       ) : (
-        <Loading />
+        <div className={styles.center}>
+          <Loading />
+        </div>
       )}
     </div>
   );
