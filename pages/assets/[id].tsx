4b366904d2360d7098722d16fa98213ccbd4ece2
NicolasCastroGarcia <nicolascastrogarcia@gmail.com>
2022-01-10 11:47:42 -0300

    adding graph in assets fixing navbar

--- pages/assets/[id].tsx
+++ pages/assets/[id].tsx
@@ -9,17 +9,18 @@ import { Network } from 'types/Network';
 import { Contract } from 'types/Contract';
 import { AuditorProvider } from 'contexts/AuditorContext';
 
-import style from './style.module.scss'
+import style from './style.module.scss';
 import useContract from 'hooks/useContract';
 import AssetTable from 'components/AssetTable';
 import { useEffect, useState } from 'react';
 import { Maturity } from 'types/Maturity';
 import AssetInfo from 'components/AssetInfo';
 
-import keys from "./translations.json";
+import keys from './translations.json';
 import { LangKeys } from 'types/Lang';
 import { useContext } from 'react';
 import LangContext from 'contexts/LangContext';
+import PoolsChart from 'components/PoolsChart';
 
 interface Props {
   walletAddress: string;
@@ -32,7 +33,9 @@ const Asset: NextPage<Props> = ({ walletAddress, network, auditor }) => {
   const translations: { [key: string]: LangKeys } = keys;
 
   const auditorContract = useContract(auditor.address, auditor.abi);
-  const [maturities, setMaturities] = useState<Array<Maturity> | undefined>(undefined);
+  const [maturities, setMaturities] = useState<Array<Maturity> | undefined>(
+    undefined
+  );
 
   async function getPools() {
     const pools = await auditorContract?.contract?.getFuturePools();
@@ -48,7 +51,7 @@ const Asset: NextPage<Props> = ({ walletAddress, network, auditor }) => {
       };
     });
 
-    setMaturities(formattedDates)
+    setMaturities(formattedDates);
   }
 
   useEffect(() => {
@@ -71,20 +74,31 @@ const Asset: NextPage<Props> = ({ walletAddress, network, auditor }) => {
               <span className={style.value}>0.19%</span>
             </div>
             <div className={style.assetMetrics}>
-              <span className={style.title}>{translations[lang].supplyAPY}</span>
+              <span className={style.title}>
+                {translations[lang].supplyAPY}
+              </span>
               <span className={style.value}>0.08%</span>
             </div>
             <div className={style.assetMetrics}>
-              <span className={style.title}>{translations[lang].distributionAPY}</span>
+              <span className={style.title}>
+                {translations[lang].distributionAPY}
+              </span>
               <span className={style.value}>0.11%</span>
             </div>
             <div className={style.assetMetrics}>
-              <span className={style.title}>{translations[lang].totalSupply}</span>
+              <span className={style.title}>
+                {translations[lang].totalSupply}
+              </span>
               <span className={style.value}>$6,123,456</span>
             </div>
           </div>
         </section>
-        <AssetTable maturities={maturities?.slice(0, 5)} />
+        <section className={style.graphContainer}>
+          <AssetTable maturities={maturities?.slice(0, 5)} />
+          <div className={style.assetGraph}>
+            <PoolsChart />
+          </div>
+        </section>
         <h2>{translations[lang].assetDetails}</h2>
         <div className={style.assetInfoContainer}>
           <AssetInfo title={translations[lang].price} value="$4,213.62" />
@@ -131,4 +145,3 @@ export async function getServerSideProps() {
     }
   };
 }
-
