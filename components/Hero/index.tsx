09803fb060ca53d837754b73d85d0ecbc4449db7
NicolasCastroGarcia <nicolascastrogarcia@gmail.com>
2022-01-11 12:09:53 -0300

    moving interval to SpecialText component

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,6 +1,8 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 
 import AssetSelector from 'components/AssetSelector';
+import PoolsChart from 'components/PoolsChart';
+import SpecialText from 'components/SpecialText';
 
 import LangContext from 'contexts/LangContext';
 
@@ -9,21 +11,10 @@ import { LangKeys } from 'types/Lang';
 import style from './style.module.scss';
 
 import keys from './translations.json';
-import PoolsChart from 'components/PoolsChart';
 
 const Hero = () => {
   const lang: string = useContext(LangContext);
   const translations: { [key: string]: LangKeys } = keys;
-
-  const [status, setStatus] = useState(true);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setStatus((status) => !status);
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <>
@@ -33,28 +24,7 @@ const Hero = () => {
             <span className={style.bolder}>DeFi</span>xed <br /> Income
           </h1>
           <p className={style.subtitle}>{translations[lang].description}</p>
-          <div className={style.specialText}>
-            <p className={style.first}>
-              <span
-                className={`${status ? style.blue : style.green} ${
-                  style.space
-                }`}
-              >
-                {status ? 'Borrow' : 'Lend'}
-              </span>{' '}
-              <span className={style.text}>knowing Exactly how</span>
-            </p>{' '}
-            <p className={style.second}>
-              <span className={style.text}>much you are gonna </span>
-              <span
-                className={`${status ? style.blue : style.green} ${
-                  style.space
-                }`}
-              >
-                {status ? 'pay' : 'earn'}
-              </span>
-            </p>
-          </div>
+          <SpecialText />
         </div>
         <div className={style.left}>
           <AssetSelector />
