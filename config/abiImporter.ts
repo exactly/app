3e9cc17df1644f4615397916516667ca13ab0606
Juani Gallo <ji.gallo93@gmail.com>
2022-03-14 15:13:57 -0300

    WIP abi importer

---
+++ config/abiImporter.ts
@@ -0,0 +1,4 @@
+import FixedLenderDAI from 'protocol/deployments/kovan/FixedLenderDAI.json';
+import FixedLenderWETH from 'protocol/deployments/kovan/FixedLenderWETH.json';
+
+export { FixedLenderDAI, FixedLenderWETH };
