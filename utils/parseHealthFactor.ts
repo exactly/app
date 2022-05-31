06c3ececeea8ef967542e96932321008f76439f2
Nicolas <nicolascastrogarcia@gmail.com>
2022-05-31 10:15:12 -0300

    fixing health factor, and exchange rate

--- utils/parseHealthFactor.ts
+++ utils/parseHealthFactor.ts
@@ -1,5 +1,5 @@
 function parseHealthFactor(debt: number, collateral: number) {
   //TODO => check case when the user doesn't have any collateral or debt
-  return collateral == 0 || debt == 0 ? '1' : (collateral / debt).toFixed(2);
+  return collateral <= 0 || debt <= 0 ? '1' : (collateral / debt).toFixed(2);
 }
 export default parseHealthFactor;
