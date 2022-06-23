667a6aadbab226f151340638c63c4b527bd0f910
NicolasCastroGarcia <nicolascastrogarcia@gmail.com>
2022-06-23 12:03:21 -0300

    adding custom error pages

---
+++ pages/500.tsx
@@ -0,0 +1,15 @@
+import Navbar from 'components/Navbar';
+import ErrorPageMessage from 'components/ErrorPageMessage';
+
+export default function Custom500() {
+  return (
+    <section>
+      <Navbar />
+      <ErrorPageMessage
+        code={500}
+        description={'Internal Server Error'}
+        message={'The page you are looking for is not available.'}
+      />
+    </section>
+  );
+}
