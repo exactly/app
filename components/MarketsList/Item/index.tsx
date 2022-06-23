bdcea301b45ec5dd9df5acaa27e3495332c993d3
NicolasCastroGarcia <nicolascastrogarcia@gmail.com>
2022-06-23 10:50:46 -0300

    changing interaction of click in tables

--- components/MarketsList/Item/index.tsx
+++ components/MarketsList/Item/index.tsx
@@ -145,12 +145,12 @@ function Item({ market, showModal, type }: Props) {
   }
 
   return (
-    <Link href={`/assets/${market?.symbol.toLowerCase()}`}>
-      <div
-        className={`${style.container} ${
-          type == 'borrow' ? style.secondaryContainer : style.primaryContainer
-        }`}
-      >
+    <div
+      className={`${style.container} ${
+        type == 'borrow' ? style.secondaryContainer : style.primaryContainer
+      }`}
+    >
+      <Link href={`/assets/${market?.symbol.toLowerCase()}`}>
         <div className={style.symbol}>
           {(market && (
             <img
@@ -163,32 +163,32 @@ function Item({ market, showModal, type }: Props) {
             {(market && parseSymbol(market?.symbol)) || <Skeleton width={30} />}
           </span>
         </div>
-        <p className={style.value}>
-          {poolData && market ? (
-            type == 'borrow' ? (
-              `$${formatNumber(poolData?.borrowed! * poolData?.rate!, 'USD')}`
-            ) : (
-              `$${formatNumber(poolData?.supplied! * poolData?.rate!, 'USD')}`
-            )
+      </Link>
+      <p className={style.value}>
+        {poolData && market ? (
+          type == 'borrow' ? (
+            `$${formatNumber(poolData?.borrowed! * poolData?.rate!, 'USD')}`
           ) : (
-            <Skeleton />
-          )}
-        </p>
-        <p className={style.value}>{(rate && `${rate}%`) || <Skeleton />}</p>
-        <div className={style.buttonContainer}>
-          {(market && (
-            <Button
-              text={type == 'borrow' ? translations[lang].borrow : translations[lang].deposit}
-              onClick={(e) => {
-                e.stopPropagation();
-                handleClick();
-              }}
-              className={type == 'borrow' ? 'secondary' : 'primary'}
-            />
-          )) || <Skeleton height={40} />}
-        </div>
+            `$${formatNumber(poolData?.supplied! * poolData?.rate!, 'USD')}`
+          )
+        ) : (
+          <Skeleton />
+        )}
+      </p>
+      <p className={style.value}>{(rate && `${rate}%`) || <Skeleton />}</p>
+      <div className={style.buttonContainer}>
+        {(market && (
+          <Button
+            text={type == 'borrow' ? translations[lang].borrow : translations[lang].deposit}
+            onClick={(e) => {
+              e.stopPropagation();
+              handleClick();
+            }}
+            className={type == 'borrow' ? 'secondary' : 'primary'}
+          />
+        )) || <Skeleton height={40} />}
       </div>
-    </Link>
+    </div>
   );
 }
 
