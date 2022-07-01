fa6a1711cab763afa862d0ffc4e785df16416195
franm <fran.marienhoff@gmail.com>
2022-07-01 18:09:41 -0300

    remove kovan info

--- utils/getSubgraph.ts
+++ utils/getSubgraph.ts
@@ -4,7 +4,6 @@ function getSubgraph(network: string | undefined) {
   const currentNetwork = network ?? process.env.NEXT_PUBLIC_NETWORK;
 
   const dictionary: Dictionary<string> = {
-    kovan: 'https://api.thegraph.com/subgraphs/name/exactly-finance/exactly-kovan',
     rinkeby: 'https://api.thegraph.com/subgraphs/name/exactly-finance/exactly-rinkeby',
     mainnet: 'https://api.thegraph.com/subgraphs/name/exactly-finance/exactly'
   };
