6623da8ccbd8de9cbfccf9ffe7259b7bac2181c2
franm <fran.marienhoff@gmail.com>
2022-07-08 17:59:04 -0300

    average apy on variable pool

--- utils/getSmartPoolInterestRate.tsx
+++ utils/getSmartPoolInterestRate.tsx
@@ -7,38 +7,72 @@ import getSubgraph from './getSubgraph';
 
 async function getSmartPoolInterestRate(network: string, fixedLenderAddress: string) {
   const oneDay = 86400;
-
+  const days = fixedLenderAddress == '0x9f275F6D25232FFf082082a53C62C6426c1cc94C' ? 10 : 3;
   const currentTimestamp = Math.floor(Date.now() / 1000);
 
   const subgraphUrl = getSubgraph(network);
 
   const smartPoolAccruedEarnings = await request(
     subgraphUrl,
-    getSmartPoolAccruedEarnings(currentTimestamp - oneDay * 3, currentTimestamp, fixedLenderAddress)
+    getSmartPoolAccruedEarnings(
+      currentTimestamp - oneDay * days,
+      currentTimestamp,
+      fixedLenderAddress
+    )
   );
 
   if (smartPoolAccruedEarnings.smartPoolEarningsAccrueds.length == 0) return '0.00';
 
   let periodAccrued = 0;
-  const newOperation = smartPoolAccruedEarnings.smartPoolEarningsAccrueds[0];
+  let totalAssetsByTime = 0;
 
-  const oldOperation =
-    smartPoolAccruedEarnings.smartPoolEarningsAccrueds[
-      smartPoolAccruedEarnings.smartPoolEarningsAccrueds.length - 1
-    ];
-
-  smartPoolAccruedEarnings.smartPoolEarningsAccrueds.forEach((event: any) => {
-    periodAccrued += parseFloat(ethers.utils.formatEther(event.earnings));
+  const reorder = smartPoolAccruedEarnings.smartPoolEarningsAccrueds.sort((a: any, b: any) => {
+    return parseFloat(a.timestamp) - parseFloat(b.timestamp);
   });
 
-  const interestRate =
-    (periodAccrued / parseFloat(ethers.utils.formatEther(oldOperation.previousAssets))) *
-    ((oneDay * 365) / (newOperation.timestamp - oldOperation.timestamp)) *
-    100;
+  const allIr = []; //for the demo
 
-  if (interestRate === Infinity || isNaN(interestRate)) return '0.00';
+  for (let i = 0; i < reorder.length - 1; i++) {
+    const element = reorder[i];
 
-  return interestRate.toFixed(2);
+    const assets = parseFloat(ethers.utils.formatEther(element.previousAssets));
+    const timestamp = parseFloat(element.timestamp);
+    const nextTimestamp = parseFloat(reorder[i + 1].timestamp);
+    const earnings = parseFloat(ethers.utils.formatEther(element.earnings));
+    const seconds = nextTimestamp - timestamp;
+
+    totalAssetsByTime += assets * seconds;
+    periodAccrued += earnings;
+
+    const totalAssets = totalAssetsByTime / (oneDay * days); //for the demo
+    const interestRate = (periodAccrued / totalAssets) * ((oneDay * 365) / (oneDay * days)) * 100; // for the demo
+    allIr.push(interestRate); //for the demo
+  }
+
+  const allIrSorted = allIr.sort((a, b) => {
+    return a - b;
+  });
+
+  let averageInterestrate = 0; //for the demo
+
+  const half = Math.floor(allIrSorted.length / 2); //for the demo
+
+  if (allIrSorted.length % 2) {
+    //for the demo
+    averageInterestrate = allIrSorted[half];
+  } else {
+    averageInterestrate = (allIrSorted[half - 1] + allIrSorted[half]) / 2.0;
+  }
+
+  if (averageInterestrate === Infinity || isNaN(averageInterestrate)) return '0.00';
+
+  return averageInterestrate.toFixed(2); //for the demo
+
+  // const totalAssets = totalAssetsByTime / (oneDay * days);
+  // const interestRate = (periodAccrued / totalAssets) * ((oneDay * 365) / (oneDay * days)) * 100;
+
+  // if (interestRate === Infinity || isNaN(interestRate)) return '0.00';
+  // return interestRate.toFixed(2);
 }
 
 export default getSmartPoolInterestRate;
