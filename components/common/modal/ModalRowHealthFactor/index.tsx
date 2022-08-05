aff12649082794bb725517f112c571b76f68e104
franm <fran.marienhoff@gmail.com>
2022-08-05 12:25:55 -0300

    add floating borrows to calculations

--- components/common/modal/ModalRowHealthFactor/index.tsx
+++ components/common/modal/ModalRowHealthFactor/index.tsx
@@ -15,6 +15,7 @@ import AccountDataContext from 'contexts/AccountDataContext';
 import styles from './style.module.scss';
 
 import keys from './translations.json';
+import getHealthFactorData from 'utils/getHealthFactorData';
 
 type Props = {
   qty: string;
@@ -46,43 +47,7 @@ function ModalRowHealthFactor({ qty, symbol, operation, healthFactorCallback }: 
   function getHealthFactor() {
     if (!accountData) return;
 
-    let collateral = 0;
-    let debt = 0;
-
-    const data = Object.values(accountData!);
-    data.forEach((fixedLender: FixedLenderAccountData) => {
-      const decimals = fixedLender.decimals;
-
-      const oracle = parseFloat(ethers.utils.formatUnits(fixedLender.oraclePrice, 18));
-      const collateralFactor = parseFloat(ethers.utils.formatUnits(fixedLender.adjustFactor, 18));
-
-      if (fixedLender.isCollateral) {
-        const assets = parseFloat(
-          ethers.utils.formatUnits(fixedLender.floatingDepositAssets, decimals)
-        );
-
-        collateral += assets * oracle * collateralFactor;
-      }
-
-      fixedLender.fixedBorrowPositions.forEach((borrowPosition) => {
-        const penaltyRate = parseFloat(ethers.utils.formatUnits(fixedLender.penaltyRate, 18));
-        const principal = parseFloat(
-          ethers.utils.formatUnits(borrowPosition.position.principal, decimals)
-        );
-        const fee = parseFloat(ethers.utils.formatUnits(borrowPosition.position.fee, decimals));
-        const maturityTimestamp = borrowPosition.maturity.toNumber();
-        const currentTimestamp = new Date().getTime() / 1000;
-
-        debt += (principal + fee) * oracle;
-        if (maturityTimestamp > currentTimestamp) {
-          debt += (currentTimestamp - maturityTimestamp) * penaltyRate;
-        }
-
-        debt = debt / collateralFactor;
-      });
-    });
-
-    const healthFactor = { collateral, debt };
+    const healthFactor = getHealthFactorData(accountData);
 
     setHealthFactor(healthFactor);
 
