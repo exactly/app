cd69f0b5387f3ed707502ef9caec40dd331c1eaa
NicolasCastroGarcia <nicolascastrogarcia@gmail.com>
2022-09-05 11:12:56 -0300

    adding security headers

--- next.config.js
+++ next.config.js
@@ -5,6 +5,33 @@
 
 const { withSentryConfig } = require('@sentry/nextjs');
 
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin'
+  }
+];
+
 const moduleExports = {
   reactStrictMode: true,
   async redirects() {
@@ -13,6 +40,14 @@ const moduleExports = {
         source: '/',
         destination: '/markets',
         permanent: true
+      }
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders
       }
     ];
   }
