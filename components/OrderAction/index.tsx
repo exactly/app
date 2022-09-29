f6ca345a9fc3b308f35a279c53ef502164d9e8e0
itofarina <farinalvaro@gmail.com>
2022-09-29 16:25:21 -0300

    feat: adds plus icon button + minor fixes

--- components/OrderAction/index.tsx
+++ components/OrderAction/index.tsx
@@ -4,6 +4,8 @@ import Box from '@mui/system/Box';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import Grid from '@mui/material/Grid';
 import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
 
 import ModalStatusContext from 'contexts/ModalStatusContext';
 import LangContext from 'contexts/LangContext';
@@ -33,11 +35,11 @@ const OrderOrDeposit: FC<OrderOrDepositProps> = ({ showActions }) => {
 
 function OrderAction() {
   const { setOpen, setOperation } = useContext(ModalStatusContext);
-  const [showActions, setShowActions] = useState(false);
+  const [showActions, setShowActions] = useState(true);
   const lang: string = useContext(LangContext);
   const translations: { [key: string]: LangKeys } = keys;
 
-  const handleAction = (action: 'deposit' | 'borrow') => {
+  const handleAction = (action: 'deposit' | 'borrow' | 'depositAtMaturity') => {
     setOperation(action);
     setOpen(true);
   };
@@ -54,11 +56,20 @@ function OrderAction() {
             <OrderOrDeposit showActions={showActions} />
           </Button>
         </Grid>
-        <Grid item display={showActions ? '' : 'none'}>
+        <Grid item display={showActions ? 'flex' : 'none'}>
           <Collapse in={showActions} orientation="horizontal" collapsedSize={0}>
-            <Button variant="outlined" onClick={() => handleAction('borrow')}>
-              {translations[lang].borrow}
-            </Button>
+            <Box sx={{ display: 'flex' }}>
+              <Button variant="outlined" onClick={() => handleAction('borrow')}>
+                {translations[lang].borrow}
+              </Button>
+              <IconButton
+                onClick={() => handleAction('depositAtMaturity')}
+                sx={{ backgroundColor: 'white', ml: 2 }}
+                size="small"
+              >
+                <AddIcon color="primary" />
+              </IconButton>
+            </Box>
           </Collapse>
         </Grid>
       </Grid>
