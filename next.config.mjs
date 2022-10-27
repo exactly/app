5c346fb30b5bedde595610c198dbfed7f5468a9d
danilo neves cruz <cruzdanilo@gmail.com>
2022-10-26 23:04:01 -0300

    🔧 enable production sourcemaps

--- next.config.mjs
+++ next.config.mjs
@@ -1,54 +1,50 @@
 import { withSentryConfig } from '@sentry/nextjs';
 
-export default withSentryConfig(
-  {
-    reactStrictMode: true,
+export default withSentryConfig({
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true,
 
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/markets',
-          permanent: true,
-        },
-      ];
-    },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/markets',
+        permanent: true,
+      },
+    ];
+  },
 
-    async headers() {
-      return [
-        {
-          source: '/:path*',
-          headers: [
-            {
-              key: 'X-DNS-Prefetch-Control',
-              value: 'on',
-            },
-            {
-              key: 'X-XSS-Protection',
-              value: '1; mode=block',
-            },
-            {
-              key: 'X-Frame-Options',
-              value: 'SAMEORIGIN',
-            },
-            {
-              key: 'Permissions-Policy',
-              value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
-            },
-            {
-              key: 'X-Content-Type-Options',
-              value: 'nosniff',
-            },
-            {
-              key: 'Referrer-Policy',
-              value: 'strict-origin',
-            },
-          ],
-        },
-      ];
-    },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin',
+          },
+        ],
+      },
+    ];
   },
-  {
-    silent: true,
-  },
-);
+});
