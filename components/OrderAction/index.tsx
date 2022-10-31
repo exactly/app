6bb0bb27006fd4d873e2077a186af244ae4dc55f
itofarina <farinalvaro@gmail.com>
2022-10-31 15:02:25 -0300

    💄 remove animation on order action

--- components/OrderAction/index.tsx
+++ components/OrderAction/index.tsx
@@ -1,11 +1,7 @@
-import React, { FC, useContext, useState } from 'react';
+import React, { FC, useContext } from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/system/Box';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import Grid from '@mui/material/Grid';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import AddIcon from '@mui/icons-material/Add';
 
 import ModalStatusContext from 'contexts/ModalStatusContext';
 import LangContext from 'contexts/LangContext';
@@ -17,27 +13,9 @@ import styles from './style.module.scss';
 
 import keys from './translations.json';
 
-type OrderOrDepositProps = {
-  showActions: boolean;
-};
-
-const OrderOrDeposit: FC<OrderOrDepositProps> = ({ showActions }) => {
-  const lang: string = useContext(LangContext);
-  const translations: { [key: string]: LangKeys } = keys;
-
-  if (showActions) return <>{translations[lang].deposit}</>;
-  return (
-    <>
-      {translations[lang].order}
-      <PlayArrowIcon sx={{ ml: 3 }} />
-    </>
-  );
-};
-
-function OrderAction() {
+const OrderAction: FC = () => {
   const { walletAddress, connect } = useWeb3Context();
   const { setOpen, setOperation } = useContext(ModalStatusContext);
-  const [showActions, setShowActions] = useState(false);
   const lang: string = useContext(LangContext);
   const translations: { [key: string]: LangKeys } = keys;
 
@@ -49,36 +27,21 @@ function OrderAction() {
   };
 
   return (
-    <Box
-      className={styles.container}
-      onMouseLeave={() => setShowActions(false)}
-      onMouseOver={() => setShowActions(true)}
-    >
+    <Box className={styles.container}>
       <Grid container spacing={2}>
         <Grid item>
           <Button variant="contained" onClick={() => handleAction('deposit')}>
-            <OrderOrDeposit showActions={showActions} />
+            {translations[lang].deposit}
           </Button>
         </Grid>
-        <Grid item display={showActions ? 'flex' : 'none'}>
-          <Collapse in={showActions} orientation="horizontal" collapsedSize={0}>
-            <Box sx={{ display: 'flex' }}>
-              <Button variant="outlined" onClick={() => handleAction('borrow')}>
-                {translations[lang].borrow}
-              </Button>
-              <IconButton
-                onClick={() => handleAction('depositAtMaturity')}
-                sx={{ backgroundColor: 'white', ml: 2 }}
-                size="small"
-              >
-                <AddIcon color="primary" />
-              </IconButton>
-            </Box>
-          </Collapse>
+        <Grid item display="flex">
+          <Button variant="outlined" onClick={() => handleAction('borrow')}>
+            {translations[lang].borrow}
+          </Button>
         </Grid>
       </Grid>
     </Box>
   );
-}
+};
 
 export default OrderAction;
