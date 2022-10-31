8984c3321686e60c4521d74b8b8ba3ddae8bc8cc
itofarina <farinalvaro@gmail.com>
2022-10-31 15:34:42 -0300

    🐛 fix view tx at etherscan

--- utils/network.ts
+++ utils/network.ts
@@ -1,6 +1,9 @@
 import networkData from 'config/networkData.json';
 
-export type Network = 'rinkeby' | 'goerli' | 'mainnet';
+export type Network = 'rinkeby' | 'goerli' | 'mainnet' | 'homestead';
 
 export const getTokenEtherscanUrl = (network: Network, address: string): string =>
   `${networkData[network].etherscanBaseUrl}/token/${address}`;
+
+export const getTxEtherscanUrl = (network: Network, txHash: string): string =>
+  `${networkData[network].etherscanBaseUrl}/tx/${txHash}`;
