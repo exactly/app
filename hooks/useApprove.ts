56cb775a65dcb9de561e3a47a4b5513f765154f1
itofarina <farinalvaro@gmail.com>
2022-11-08 12:57:51 -0300

    🎨 extract useApprove hook

---
+++ hooks/useApprove.ts
@@ -0,0 +1,41 @@
+import { parseFixed } from '@ethersproject/bignumber';
+import { MaxUint256, WeiPerEther } from '@ethersproject/constants';
+import { ErrorCode } from '@ethersproject/logger';
+import { captureException } from '@sentry/nextjs';
+import { useCallback, useState } from 'react';
+import { ERC20, Market } from 'types/contracts';
+import { ErrorData } from 'types/Error';
+import numbers from 'config/numbers.json';
+
+export default (contract?: ERC20 | Market, spender?: string) => {
+  const [errorData, setErrorData] = useState<ErrorData | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const approve = useCallback(async () => {
+    if (!contract || !spender) return;
+
+    try {
+      setIsLoading(true);
+      const gasEstimation = await contract.estimateGas.approve(spender, MaxUint256);
+
+      const approveTx = await contract.approve(spender, MaxUint256, {
+        gasLimit: gasEstimation.mul(parseFixed(String(numbers.gasLimitMultiplier), 18)).div(WeiPerEther),
+      });
+
+      await approveTx.wait();
+    } catch (error: any) {
+      const isDenied = error?.code === ErrorCode.ACTION_REJECTED;
+
+      if (!isDenied) captureException(error);
+
+      setErrorData({
+        status: true,
+        message: isDenied ? 'Transaction rejected' : 'Approve failed, please try again',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }, [spender, contract]);
+
+  return { approve, isLoading, errorData };
+};
