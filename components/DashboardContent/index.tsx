4d6c56b09679aef59a851d26d061e4a4db18a1bf
Juan Pablo Rombola <juampi.rombola@gmail.com>
2022-11-16 09:04:54 -0300

    ♻️ change tabs to mui tabs

--- components/DashboardContent/index.tsx
+++ components/DashboardContent/index.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { globals } from 'styles/theme';
 
-import Tabs from 'components/Tabs';
+const { maxWidth } = globals;
 
 // const DashboardUserCharts = dynamic(() => import('components/DashboardUserCharts'));
 const SmartPoolDashboard = dynamic(() => import('components/SmartPoolDashboard'));
@@ -10,48 +17,66 @@ const EmptyState = dynamic(() => import('components/EmptyState'));
 
 import { useWeb3Context } from 'contexts/Web3Context';
 
-import { Option } from 'react-dropdown';
-
-import translations from 'dictionary/en.json';
-
-import styles from './style.module.scss';
-
 function DashboardContent() {
   const { walletAddress } = useWeb3Context();
 
-  const tabDeposit = {
-    label: translations.deposit,
-    value: 'deposit',
-  };
+  const allTabs = [
+    {
+      label: 'Your Deposits',
+      value: 'deposit',
+    },
+    {
+      label: 'Your Borrows',
+      value: 'borrow',
+    },
+  ];
 
-  const tabBorrow = {
-    label: translations.borrow,
-    value: 'borrow',
-  };
+  const [tabValue, setTabValue] = useState<string>(allTabs[0].value);
 
-  const [tab, setTab] = useState<Option>(tabDeposit);
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setTabValue(newValue);
+  };
 
   return (
-    <section className={styles.container}>
-      {/* <div className={styles.chartContainer}>{<DashboardUserCharts />}</div> */}
-      <div className={styles.poolsContainer}>
-        <Tabs
-          values={[tabDeposit, tabBorrow]}
-          selected={tab}
-          handleTab={(value: Option) => {
-            setTab(value);
-          }}
-        />
-        {walletAddress ? (
-          <>
-            <SmartPoolDashboard tab={tab} />
-            <MaturityPoolDashboard tab={tab} />
-          </>
-        ) : (
-          <EmptyState />
-        )}
-      </div>
-    </section>
+    <Grid container sx={{ maxWidth: maxWidth }} mx="auto" mt={5}>
+      <TabContext value={tabValue}>
+        <Box width="100%">
+          <TabList
+            onChange={handleChange}
+            aria-label="lab API tabs example"
+            TabIndicatorProps={{ sx: { height: '4px', background: '#34C53A' } }}
+            textColor="inherit"
+            sx={{
+              '& button:hover': { backgroundColor: '#5c5a5a', color: 'white' },
+              '& button:focus': { backgroundColor: '#0E0E0E', color: 'white' },
+              '& button.Mui-selected': { backgroundColor: '#0E0E0E', color: 'white' },
+            }}
+          >
+            {allTabs.map((tab) => (
+              <Tab
+                label={tab.label}
+                value={tab.value}
+                sx={{ paddingX: 5, fontSize: '1.1em' }}
+                key={`tab_${tab.value}`}
+              />
+            ))}
+          </TabList>
+          <Box sx={{ borderBottom: 4, borderColor: 'divider', marginTop: '-4px' }} width="100%" />
+        </Box>
+        {allTabs.map((tab) => (
+          <TabPanel value={tab.value} sx={{ width: '100%', padding: 0 }} key={tab.value}>
+            {walletAddress ? (
+              <>
+                <SmartPoolDashboard tab={tab} />
+                <MaturityPoolDashboard tab={tab} />
+              </>
+            ) : (
+              <EmptyState />
+            )}
+          </TabPanel>
+        ))}
+      </TabContext>
+    </Grid>
   );
 }
 
