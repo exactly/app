86aa5832d28588f33c4c4ab6d1220ef792d46d6d
itofarina <farinalvaro@gmail.com>
2022-11-17 13:46:49 -0300

    🐛 editable row: keep value as string on change

--- components/common/modal/ModalRowEditable/index.tsx
+++ components/common/modal/ModalRowEditable/index.tsx
@@ -42,7 +42,7 @@ function ModalRowEditable({ text, value, line, editable, symbol, placeholder, on
               min={0.0}
               type="number"
               placeholder={placeholder ?? '0.00'}
-              value={value && parseFloat(value)}
+              value={value}
               onChange={onChange}
               name={text}
               className={styles.input}
