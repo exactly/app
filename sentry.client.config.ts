fd3a6233fe7502ade6b54b904eda13971e03d681
danilo neves cruz <cruzdanilo@gmail.com>
2022-11-21 12:01:20 -0300

    🥅 sentry: add extra error data

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,7 +1,10 @@
-// This file configures the initialization of Sentry on the browser.
-// The config you add here will be used whenever a page is visited.
 import * as Sentry from '@sentry/nextjs';
+import { ExtraErrorData } from '@sentry/integrations';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init({ dsn: SENTRY_DSN, tracesSampleRate: 1.0 });
+Sentry.init({
+  dsn: SENTRY_DSN,
+  tracesSampleRate: 1.0,
+  integrations: [new ExtraErrorData({ depth: 5 })],
+});
