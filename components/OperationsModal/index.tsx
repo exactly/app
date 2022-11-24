ef40c17a5c9f0071d76d69a8bdf62743659e12c3
franm <fran.marienhoff@gmail.com>
2022-11-24 16:38:20 -0300

    ✨ add close button modal

--- components/OperationsModal/index.tsx
+++ components/OperationsModal/index.tsx
@@ -5,7 +5,8 @@ import OperationContainer from './OperationContainer';
 
 import ModalStatusContext from 'contexts/ModalStatusContext';
 
-import { Dialog, DialogContent } from '@mui/material';
+import { Dialog, DialogContent, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 function OperationsModals() {
   const { open, setOpen } = useContext(ModalStatusContext);
@@ -13,6 +14,18 @@ function OperationsModals() {
   return (
     <Dialog open={open} onClose={() => setOpen(false)}>
       <DialogContent sx={{ display: 'flex' }}>
+        <IconButton
+          aria-label="close"
+          onClick={() => setOpen(false)}
+          sx={{
+            position: 'absolute',
+            right: 10,
+            top: 10,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
         <OperationsSelector />
         <OperationContainer />
       </DialogContent>
