488ef8b39160d893f64045e2c01e5c353f77ab0a
Juan Pablo Rombola <juampi.rombola@gmail.com>
2022-11-24 12:51:57 -0300

    ♻️ Add useMaturityPoolTransactions hook

--- components/dashboard/DashboardContent/MaturityPoolDashboard/MaturityPoolDashboardTable/CollapseMaturityPool/index.tsx
+++ components/dashboard/DashboardContent/MaturityPoolDashboard/MaturityPoolDashboardTable/CollapseMaturityPool/index.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { Box, Collapse, Skeleton, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
-
-type Transaction = {
-  id: string;
-  type: string;
-  date: string;
-  amount: string;
-  amountUSD: string;
-  isBorrowOrDeposit: boolean;
-};
+import { Collapse, Skeleton, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
+import { MaturityPoolTransaction } from 'types/MaturityPoolTransaction';
 
 type Props = {
   open: boolean;
-  transactions: Transaction[];
+  transactions: MaturityPoolTransaction[];
 };
 
 const headers: {
@@ -36,54 +28,50 @@ const headers: {
 function CollapseMaturityPool({ open, transactions }: Props) {
   return (
     <Collapse in={open} timeout="auto" unmountOnExit>
-      <Box p={2}>
-        <Table size="small" aria-label="purchases">
-          <TableHead sx={{ backgroundColor: '#E7E7E7' }}>
-            <TableRow>
-              {headers.map(({ label, align }) => (
-                <TableCell key={`collapse_maturity_header_${label}`} align={align}>
-                  <Typography variant="subtitle2" sx={{ color: 'grey.600' }} fontWeight={600}>
-                    {label}
-                  </Typography>
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {transactions?.map(({ id, date, type, amount, amountUSD, isBorrowOrDeposit }) => (
-              <TableRow key={`collapsed_${id}`} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} hover>
-                <TableCell component="th" align="left" size="small">
-                  <Typography variant="body2">
-                    {<div>{date}</div> || <Skeleton sx={{ margin: 'auto' }} width={40} />}
-                  </Typography>
-                </TableCell>
-                <TableCell align="center" size="small">
-                  <Typography variant="body2">
-                    {(
-                      <div>
-                        <span style={isBorrowOrDeposit ? { color: `var(--success)` } : { color: `var(--error)` }}>
-                          {isBorrowOrDeposit ? '↓' : '↑'}
-                        </span>
-                        <span style={{ paddingLeft: '2px' }}>{type}</span>
-                      </div>
-                    ) || <Skeleton sx={{ margin: 'auto' }} width={40} />}
-                  </Typography>
-                </TableCell>
-                <TableCell align="right" size="small">
-                  <Typography variant="body2">
-                    {(
-                      <div>
-                        {amount}
-                        <span style={{ fontSize: '0.9em', color: 'grey', paddingLeft: '4px' }}>(${amountUSD})</span>
-                      </div>
-                    ) || <Skeleton sx={{ margin: 'auto' }} width={40} />}
-                  </Typography>
-                </TableCell>
-              </TableRow>
+      <Table size="small" aria-label="purchases">
+        <TableHead sx={{ backgroundColor: '#ebebeb' }}>
+          <TableRow>
+            {headers.map(({ label, align }) => (
+              <TableCell key={`collapse_maturity_header_${label}`} align={align}>
+                <Typography variant="subtitle2" sx={{ color: 'grey.600' }} fontWeight={600}>
+                  {label}
+                </Typography>
+              </TableCell>
             ))}
-          </TableBody>
-        </Table>
-      </Box>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {transactions?.map(({ id, date, type, amount, amountUSD, isBorrowOrDeposit }) => (
+            <TableRow key={`collapsed_${id}`} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} hover>
+              <TableCell component="th" align="left" size="small">
+                <Typography variant="body2">{date || <Skeleton sx={{ margin: 'auto' }} width={40} />}</Typography>
+              </TableCell>
+              <TableCell align="center" size="small">
+                <Typography variant="body2">
+                  {(
+                    <>
+                      <span style={isBorrowOrDeposit ? { color: `var(--success)` } : { color: `var(--error)` }}>
+                        {isBorrowOrDeposit ? '↓' : '↑'}
+                      </span>
+                      <span style={{ paddingLeft: '2px' }}>{type}</span>
+                    </>
+                  ) || <Skeleton sx={{ margin: 'auto' }} width={40} />}
+                </Typography>
+              </TableCell>
+              <TableCell align="right" size="small">
+                <Typography variant="body2">
+                  {(
+                    <>
+                      {amount}
+                      <span style={{ fontSize: '0.9em', color: 'grey', paddingLeft: '4px' }}>(${amountUSD})</span>
+                    </>
+                  ) || <Skeleton sx={{ margin: 'auto' }} width={40} />}
+                </Typography>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </Collapse>
   );
 }
