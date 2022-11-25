66a4ac1f3e33a92fd2a7f67cbdfc60da64c4446c
Juan Pablo Rombola <juampi.rombola@gmail.com>
2022-11-25 16:02:26 -0300

    🩹 add accountData dependency to subgraph query hook

--- hooks/useFixedPoolTransactions.ts
+++ hooks/useFixedPoolTransactions.ts
@@ -1,10 +1,11 @@
+import AccountDataContext from 'contexts/AccountDataContext';
 import { useWeb3Context } from 'contexts/Web3Context';
 import request from 'graphql-request';
 import { getMaturityPoolBorrowsQuery } from 'queries/getMaturityPoolBorrows';
 import { getMaturityPoolDepositsQuery } from 'queries/getMaturityPoolDeposits';
 import { getMaturityPoolRepaysQuery } from 'queries/getMaturityPoolRepay';
 import { getMaturityPoolWithdrawsQuery } from 'queries/getMaturityPoolWithdraw';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { Borrow } from 'types/Borrow';
 import { Deposit } from 'types/Deposit';
 import { Repay } from 'types/Repay';
@@ -12,6 +13,7 @@ import { WithdrawMP } from 'types/WithdrawMP';
 import getSubgraph from 'utils/getSubgraph';
 
 export default (type: 'borrow' | 'deposit', maturity: string, market: string) => {
+  const { accountData } = useContext(AccountDataContext);
   const { walletAddress, network } = useWeb3Context();
   const [withdrawTxs, setWithdrawTxs] = useState<WithdrawMP[]>([]);
   const [repayTxs, setRepayTxs] = useState<Repay[]>([]);
@@ -20,7 +22,7 @@ export default (type: 'borrow' | 'deposit', maturity: string, market: string) =>
 
   const getFixedPoolTransactions = useCallback(async () => {
     const subgraphUrl = getSubgraph(network?.name);
-    if (!walletAddress || !maturity || !market || !type || !subgraphUrl) return;
+    if (!walletAddress || !maturity || !market || !type || !subgraphUrl || !accountData) return;
 
     if (type === 'borrow') {
       const getMaturityPoolBorrows = await request(
@@ -47,7 +49,7 @@ export default (type: 'borrow' | 'deposit', maturity: string, market: string) =>
       );
       setWithdrawTxs([...getMaturityPoolWithdraws.withdrawAtMaturities]);
     }
-  }, [market, maturity, network?.name, type, walletAddress]);
+  }, [market, maturity, network?.name, type, walletAddress, accountData]);
 
   useEffect(() => {
     getFixedPoolTransactions();
