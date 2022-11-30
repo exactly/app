ae9453c7dff5bd335e87ded6606dd7ff562ffd67
Jorge Galat <jrgglt@gmail.com>
2022-11-30 11:58:33 -0300

    ♻️  improve account data context

--- contexts/AccountDataContext.tsx
+++ contexts/AccountDataContext.tsx
@@ -1,61 +1,52 @@
-import type { FC, ReactNode } from 'react';
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { FC, useCallback, createContext, useEffect, useState, PropsWithChildren } from 'react';
 import { AddressZero } from '@ethersproject/constants';
+import { captureException } from '@sentry/nextjs';
 
 import { AccountData } from 'types/AccountData';
 import { FixedLenderAccountData } from 'types/FixedLenderAccountData';
 
 import { useWeb3Context } from './Web3Context';
-import ContractsContext from './ContractsContext';
 
-import getABI from 'config/abiImporter';
 import useDebounce from 'hooks/useDebounce';
+import usePreviewer from 'hooks/usePreviewer';
 
 type ContextValues = {
   accountData: AccountData | undefined;
-  getAccountData: () => void;
+  getAccountData: () => Promise<void>;
 };
 
 const defaultValues: ContextValues = {
   accountData: undefined,
-  getAccountData: () => undefined,
+  getAccountData: async () => undefined,
 };
 
 const AccountDataContext = createContext(defaultValues);
 
-export const AccountDataProvider: FC<{ children?: ReactNode }> = ({ children }) => {
-  const [accountData, setAccountData] = useState<AccountData | undefined>(undefined);
-  const { network, walletAddress } = useWeb3Context();
+export const AccountDataProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [accountData, setAccountData] = useState<AccountData | undefined>();
+  const { walletAddress } = useWeb3Context();
+  const Previewer = usePreviewer();
 
   const walletAddressDebounced = useDebounce(walletAddress);
 
-  const { getInstance } = useContext(ContractsContext);
-  const { Previewer } = getABI(network?.name);
+  const getAccountData = useCallback(async () => {
+    if (!Previewer) return;
+
+    const wallet = walletAddressDebounced ? walletAddressDebounced : AddressZero;
+
+    const data = await Previewer?.exactly(wallet);
+
+    setAccountData(Object.fromEntries(data.map((market: FixedLenderAccountData) => [market.assetSymbol, market])));
+  }, [Previewer, walletAddressDebounced]);
 
   useEffect(() => {
-    getAccountData();
-  }, [walletAddressDebounced, Previewer]);
+    void getAccountData().catch(captureException);
+  }, [getAccountData]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      getAccountData();
-    }, 600000);
+    const interval = setInterval(() => void getAccountData().catch(captureException), 600000);
     return () => clearInterval(interval);
-  }, [walletAddressDebounced, Previewer]);
-
-  async function getAccountData() {
-    try {
-      const previewerContract = getInstance(Previewer.address!, Previewer.abi!, 'previewer');
-
-      const wallet = walletAddressDebounced ? walletAddressDebounced : AddressZero;
-
-      const data = await previewerContract?.exactly(wallet);
-
-      setAccountData(Object.fromEntries(data.map((market: FixedLenderAccountData) => [market.assetSymbol, market])));
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  }, [getAccountData]);
 
   return <AccountDataContext.Provider value={{ accountData, getAccountData }}>{children}</AccountDataContext.Provider>;
 };
