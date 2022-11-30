ae32755256c4a6863073cda274947175eca90c6b
Jorge Galat <jrgglt@gmail.com>
2022-11-30 11:58:33 -0300

    ♻️  improve account data context

--- utils/formatNumber.ts
+++ utils/formatNumber.ts
@@ -1,13 +1,13 @@
+const dictionary: Record<string, number> = {
+  USD: 2,
+  DAI: 2,
+  USDC: 2,
+  WETH: 6,
+  WBTC: 6,
+};
+
 function formatNumber(number: string | number, symbol?: string, standard?: boolean) {
   const parsedNumber = typeof number === 'string' ? parseFloat(number) : number;
-
-  const dictionary: Record<string, number> = {
-    USD: 2,
-    DAI: 2,
-    USDC: 2,
-    WETH: 6,
-    WBTC: 6,
-  };
 
   return new Intl.NumberFormat('en-GB', {
     notation: standard ? 'standard' : 'compact',
