c7df59d3417e9fa4464a8622ee45274fdd7c3789
Jorge Galat <jrgglt@gmail.com>
2022-11-30 11:57:13 -0300

    ✨ add useAccountData hook

---
+++ hooks/useAccountData.ts
@@ -0,0 +1,8 @@
+import { useContext, useMemo } from 'react';
+import AccountDataContext from 'contexts/AccountDataContext';
+import { FixedLenderAccountData } from 'types/FixedLenderAccountData';
+
+export default function (symbol: string): Partial<FixedLenderAccountData> {
+  const { accountData } = useContext(AccountDataContext);
+  return useMemo<Partial<FixedLenderAccountData>>(() => accountData?.[symbol] ?? {}, [symbol, accountData]);
+}
