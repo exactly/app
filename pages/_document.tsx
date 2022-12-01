f9df00b12eff45bfcadcfa381ad2a0d2e6d8f07f
Jorge Galat <jrgglt@gmail.com>
2022-12-01 15:58:21 -0300

    ✨ update lang on html

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en-US">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
