3136d8766404e1e0b8790536b66e27392aafa861
danilo neves cruz <cruzdanilo@gmail.com>
2022-12-07 16:23:24 -0300

    🚨 analytics: handle undefined config

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,12 +1,15 @@
 import Analytics from 'analytics';
 import googleAnalytics from '@analytics/google-analytics';
 
+const { NEXT_PUBLIC_GOOGLE_ANALYTICS } = process.env;
+
 const analytics = Analytics({
   plugins: [
-    googleAnalytics({
-      measurementIds: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
-    }),
-  ],
+    NEXT_PUBLIC_GOOGLE_ANALYTICS &&
+      googleAnalytics({
+        measurementIds: NEXT_PUBLIC_GOOGLE_ANALYTICS,
+      }),
+  ].filter(Boolean),
 });
 
 export default analytics;
