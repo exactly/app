ebf93b8283e0ab39d033ebe90a77602e37af4674
Jorge Galat <jrgglt@gmail.com>
2022-12-12 14:18:58 -0300

    ✨ add csp header

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,7 @@ export default withSentryConfig({
         { key: 'X-DNS-Prefetch-Control', value: 'on' },
         { key: 'X-XSS-Protection', value: '1; mode=block' },
         { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+        { key: 'Content-Security-Policy', value: "frame-ancestors 'none'" },
         { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()' },
         { key: 'X-Content-Type-Options', value: 'nosniff' },
         { key: 'Referrer-Policy', value: 'strict-origin' },
