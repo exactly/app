c2aa70eb2536d5d1a08277dc8b45106e08a00185
Juan Pablo Rombola <juampi.rombola@gmail.com>
2022-12-12 15:40:02 -0300

    💄 change background color of the app

--- components/markets/MarketsTables/poolTable/index.tsx
+++ components/markets/MarketsTables/poolTable/index.tsx
@@ -116,7 +116,7 @@ const PoolTable: FC<PoolTableProps> = ({ isLoading, headers, rows, rateType }) =
   );
 
   return (
-    <TableContainer component={Paper}>
+    <TableContainer>
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
