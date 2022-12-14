79cbbf726ce297df5b08190b5fb1ab1ed4383563
Jorge Galat <jrgglt@gmail.com>
2022-12-14 14:21:27 -0300

    🔥 remove getUnderlyingData

--- pages/[symbol].tsx
+++ pages/[symbol].tsx
@@ -1,54 +1,21 @@
-import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Grid } from '@mui/material';
 import { basename } from 'path';
 import { readdir, readFile } from 'fs/promises';
 import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import type { Maturity } from 'types/Maturity';
-import { getUnderlyingData } from 'utils/utils';
-import { MarketContext } from 'contexts/MarketContext';
-import { useWeb3 } from 'hooks/useWeb3';
-import AccountDataContext from 'contexts/AccountDataContext';
 import AssetMaturityPools from 'components/asset/MaturityPool';
 import AssetFloatingPool from 'components/asset/FloatingPool';
 import OperationsModal from 'components/OperationsModal';
 import AssetHeaderInfo from 'components/asset/Header';
 import MobileNavbar from 'components/MobileNavbar';
 import Navbar from 'components/Navbar';
-import getLastAPR from 'utils/getLastAPR';
 import analytics from 'utils/analytics';
 import style from './[symbol].module.scss';
+import useAccountData from 'hooks/useAccountData';
 
 const Market: NextPage<{ symbol: string }> = ({ symbol }) => {
-  const { chain } = useWeb3();
-  const { dates } = useContext(MarketContext);
-  const { accountData } = useContext(AccountDataContext);
-
-  const networkName = (chain?.network || process.env.NEXT_PUBLIC_NETWORK) as string;
-  const assetAddress = getUnderlyingData(networkName, symbol)?.address as string;
-
-  const [, setDepositsData] = useState<Array<Maturity> | undefined>(undefined);
-  const [, setBorrowsData] = useState<Array<Maturity> | undefined>(undefined);
-
-  const market = useMemo(() => accountData?.[symbol].market, [accountData, symbol]);
-
-  const handleAPR = useCallback(async () => {
-    if (!accountData || !market || !chain) return;
-
-    try {
-      const apr: any = await getLastAPR(dates, symbol, chain.id, accountData);
-
-      const deposit = apr?.sortedDeposit;
-      const borrow = apr?.sortedBorrow;
-
-      setDepositsData(deposit);
-      setBorrowsData(borrow);
-    } catch (e) {
-      console.log(e);
-    }
-  }, [chain, accountData, market, dates]);
-
-  useEffect(() => void handleAPR(), [handleAPR]);
+  const { market } = useAccountData(symbol);
 
   useEffect(() => void analytics.page(), []);
 
@@ -57,9 +24,8 @@ const Market: NextPage<{ symbol: string }> = ({ symbol }) => {
       <OperationsModal />
       <MobileNavbar />
       <Navbar />
-
       <section className={style.container} style={{ marginTop: '130px' }}>
-        <AssetHeaderInfo symbol={symbol} assetAddress={assetAddress} eMarketAddress={market} />
+        <AssetHeaderInfo symbol={symbol} eMarketAddress={market} />
         <Grid container mt={5}>
           <Grid item container>
             <AssetFloatingPool symbol={symbol} eMarketAddress={market} />
