938c18ec4f77f481a426edc00db423fb87de5496
Jorge Galat <jrgglt@gmail.com>
2022-12-14 16:54:41 -0300

    ♻️  general code improvements

--- components/common/modal/ModalRowBorrowLimit/index.tsx
+++ components/common/modal/ModalRowBorrowLimit/index.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useMemo } from 'react';
 import Image from 'next/image';
 import Skeleton from 'react-loading-skeleton';
-import { Zero } from '@ethersproject/constants';
+import { WeiPerEther, Zero } from '@ethersproject/constants';
 import { parseFixed, formatFixed } from '@ethersproject/bignumber';
 
 import { LangKeys } from 'types/Lang';
@@ -15,6 +15,7 @@ import keys from './translations.json';
 
 import formatNumber from 'utils/formatNumber';
 import getBeforeBorrowLimit from 'utils/getBeforeBorrowLimit';
+import { checkPrecision } from 'utils/utils';
 
 type Props = {
   qty: string;
@@ -29,75 +30,63 @@ function ModalRowBorrowLimit({ qty, symbol, operation, line }: Props) {
   const translations: { [key: string]: LangKeys } = keys;
 
   const newQty = useMemo(() => {
-    return getAmount();
-  }, [accountData, symbol, qty]);
-
-  const [beforeBorrowLimit, afterBorrowLimit] = useMemo(() => {
-    return getBorrowLimits();
-  }, [accountData, symbol, newQty, operation]);
-
-  function getAmount() {
     if (!accountData || !symbol) return;
 
     if (!qty) return Zero;
 
-    const decimals = accountData[symbol].decimals;
-    const regex = /[^,.]*$/g;
-    const inputDecimals = regex.exec(qty)![0];
+    const { decimals } = accountData[symbol];
 
-    if (inputDecimals.length > decimals) return;
+    if (!checkPrecision(qty, decimals)) return;
 
-    const newQty = parseFixed(qty, decimals);
+    return parseFixed(qty, decimals);
+  }, [accountData, symbol, qty]);
 
-    return newQty;
-  }
-
-  function getBorrowLimits() {
+  const [beforeBorrowLimit, afterBorrowLimit] = useMemo(() => {
     if (!accountData || !newQty) return [undefined, undefined];
 
     const { usdPrice, decimals, adjustFactor, isCollateral } = accountData[symbol];
-
-    const WAD = parseFixed('1', 18);
 
     const beforeBorrowLimitUSD = getBeforeBorrowLimit(accountData, symbol, usdPrice, decimals, operation);
 
     const newQtyUsd = newQty.mul(usdPrice).div(parseFixed('1', decimals));
 
-    const beforeBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD, 18)).toFixed(2);
-    let afterBorrowLimit = beforeBorrowLimit;
+    const newBeforeBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD, 18)).toFixed(2);
+    let newAfterBorrowLimit = newBeforeBorrowLimit;
 
     switch (operation) {
       case 'deposit':
         if (isCollateral) {
-          const adjustedDepositBorrowLimit = newQtyUsd.mul(adjustFactor).div(WAD);
+          const adjustedDepositBorrowLimit = newQtyUsd.mul(adjustFactor).div(WeiPerEther);
 
-          afterBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD.add(adjustedDepositBorrowLimit), 18)).toFixed(2);
+          newAfterBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD.add(adjustedDepositBorrowLimit), 18)).toFixed(
+            2,
+          );
         } else {
-          afterBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD, 18)).toFixed(2);
+          newAfterBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD, 18)).toFixed(2);
         }
         break;
 
       case 'withdraw':
-        afterBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD.sub(newQtyUsd), 18)).toFixed(2);
+        newAfterBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD.sub(newQtyUsd), 18)).toFixed(2);
         break;
 
       case 'borrow':
-        afterBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD.sub(newQtyUsd), 18)).toFixed(2);
+        newAfterBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD.sub(newQtyUsd), 18)).toFixed(2);
         break;
 
       case 'repay':
         if (isCollateral) {
-          const adjustedRepayBorrowLimit = newQtyUsd.mul(adjustFactor).div(WAD);
+          const adjustedRepayBorrowLimit = newQtyUsd.mul(adjustFactor).div(WeiPerEther);
 
-          afterBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD.add(adjustedRepayBorrowLimit), 18)).toFixed(2);
+          newAfterBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD.add(adjustedRepayBorrowLimit), 18)).toFixed(2);
         } else {
-          afterBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD, 18)).toFixed(2);
+          newAfterBorrowLimit = Number(formatFixed(beforeBorrowLimitUSD, 18)).toFixed(2);
         }
         break;
     }
 
-    return [beforeBorrowLimit, afterBorrowLimit];
-  }
+    return [newBeforeBorrowLimit, newAfterBorrowLimit];
+  }, [accountData, symbol, newQty, operation]);
 
   return (
     <section className={line ? `${styles.row} ${styles.line}` : styles.row}>
