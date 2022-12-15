735789bd3c92f009513de9b581ee03a77434dfcf
Jorge Galat <jrgglt@gmail.com>
2022-12-15 18:02:48 -0300

    🔥 remove unnecessary imports

--- components/dashboard/DashboardHeader/index.tsx
+++ components/dashboard/DashboardHeader/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useMemo, useState } from 'react';
+import React, { useContext, useMemo } from 'react';
 import { BigNumber, formatFixed, parseFixed } from '@ethersproject/bignumber';
 import { Grid } from '@mui/material';
 import { Zero } from '@ethersproject/constants';
