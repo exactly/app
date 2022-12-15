137595a335f6400c87567900143e527fed0d2249
Jorge Galat <jrgglt@gmail.com>
2022-12-15 16:35:39 -0300

    🐛 fix wrong asset balance

--- hooks/useERC20.ts
+++ hooks/useERC20.ts
@@ -1,17 +1,32 @@
-import { useMemo } from 'react';
+import { useContext, useEffect, useMemo, useState } from 'react';
 import { useSigner } from 'wagmi';
 import { Contract } from '@ethersproject/contracts';
 import type { ERC20 } from 'types/contracts/ERC20';
 import erc20ABI from 'abi/ERC20.json';
+import { useOperationContext } from 'contexts/OperationContext';
+import { MarketContext } from 'contexts/MarketContext';
+import useMarket from './useMarket';
 
-export default (address?: string) => {
+export default (): ERC20 | undefined => {
   const { data: signer } = useSigner();
+  const { market } = useContext(MarketContext);
+  const { symbol } = useOperationContext();
+
+  const marketContract = useMarket(market?.value);
+
+  const [assetAddress, setAssetAddress] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (!marketContract || symbol === 'WETH') return;
+
+    void marketContract.asset().then(setAssetAddress);
+  }, [marketContract, symbol]);
 
   const assetContract = useMemo(() => {
-    if (!signer || !address) return;
+    if (!signer || !assetAddress) return;
 
-    return new Contract(address, erc20ABI, signer) as ERC20;
-  }, [address, signer]);
+    return new Contract(assetAddress, erc20ABI, signer) as ERC20;
+  }, [assetAddress, signer]);
 
   return assetContract;
 };
