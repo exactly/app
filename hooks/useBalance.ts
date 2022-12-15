e766dc79e50f13a3cdb8f6a305d02e6b99f0518a
Jorge Galat <jrgglt@gmail.com>
2022-12-15 16:35:39 -0300

    🐛 fix wrong asset balance

--- hooks/useBalance.ts
+++ hooks/useBalance.ts
@@ -4,11 +4,15 @@ import { formatFixed } from '@ethersproject/bignumber';
 import type { ERC20 } from 'types/contracts';
 import { useWeb3 } from './useWeb3';
 
-export default (symbol?: string, assetContract?: ERC20) => {
+export default (symbol?: string, assetContract?: ERC20): string | undefined => {
   const { walletAddress } = useWeb3();
   const { data } = useBalance({
     address: walletAddress as `0x${string}` | undefined,
     token: symbol === 'WETH' ? undefined : (assetContract?.address as `0x${string}` | undefined),
   });
-  return useMemo(() => data && formatFixed(data.value, data.decimals), [data]);
+  return useMemo(() => {
+    if (!data || (!assetContract?.address && symbol !== 'WETH')) return;
+
+    return formatFixed(data.value, data.decimals);
+  }, [data, assetContract?.address, symbol]);
 };
