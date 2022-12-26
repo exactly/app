d75d83d4dfd4f79a2d1d7c7a04da3491ab8ce5e0
Juan Pablo Rombola <juampi.rombola@gmail.com>
2022-12-26 09:33:26 -0300

    🎨 dehardcode chain ids

--- hooks/usePreviewer.ts
+++ hooks/usePreviewer.ts
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 import { Contract } from '@ethersproject/contracts';
-import { useProvider } from 'wagmi';
+import { goerli, mainnet, useProvider } from 'wagmi';
 import type { Previewer } from 'types/contracts/Previewer';
 import mainnetPreviewer from '@exactly-protocol/protocol/deployments/mainnet/Previewer.json' assert { type: 'json' };
 import goerliPreviewer from '@exactly-protocol/protocol/deployments/goerli/Previewer.json' assert { type: 'json' };
@@ -16,8 +16,8 @@ export default () => {
 
     // TODO: add optimism network
     const address = {
-      5: goerliPreviewer.address,
-      1: mainnetPreviewer.address,
+      [goerli.id]: goerliPreviewer.address,
+      [mainnet.id]: mainnetPreviewer.address,
     }[chain.id];
 
     if (!address) return null;
