c89a5f99b6eab27ad160034bff12e5dc4536d5c9
danilo neves cruz <cruzdanilo@gmail.com>
2022-12-27 18:47:20 -0500

    🚸 web3modal: set default chain

--- hooks/useWeb3.ts
+++ hooks/useWeb3.ts
@@ -2,7 +2,7 @@ import { useAccount, useNetwork, useSwitchNetwork } from 'wagmi';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import * as chains from 'wagmi/chains';
-import { supportedChains } from 'utils/client';
+import { defaultChain, supportedChains } from 'utils/client';
 import useDebounce from './useDebounce';
 
 export const useWeb3 = () => {
@@ -11,18 +11,13 @@ export const useWeb3 = () => {
   const { chain } = useNetwork();
   const { query } = useRouter();
   const queryChain = typeof query.n === 'string' ? chains[query.n as keyof typeof chains] : undefined;
+  const isQueryChainSupported = queryChain && supportedChains.find(({ id }) => queryChain?.id === id);
 
   useEffect(() => {
-    if (
-      switchNetwork &&
-      chain &&
-      queryChain &&
-      chain.id !== queryChain.id &&
-      supportedChains.find(({ id }) => queryChain?.id === id)
-    ) {
+    if (switchNetwork && isQueryChainSupported && chain && chain.id !== queryChain.id) {
       switchNetwork(queryChain.id);
     }
-  }, [chain, queryChain, switchNetwork]);
+  }, [chain, isQueryChainSupported, queryChain?.id, switchNetwork]);
 
   const walletAddress = useDebounce(address);
   return {
@@ -32,6 +27,8 @@ export const useWeb3 = () => {
     chain:
       walletAddress && supportedChains.find(({ id }) => chain?.id === id)
         ? chain
-        : queryChain ?? chains[process.env.NEXT_PUBLIC_NETWORK as keyof typeof chains],
+        : isQueryChainSupported
+        ? queryChain
+        : defaultChain,
   };
 };
