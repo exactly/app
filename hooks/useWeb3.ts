55e8d8d4ecf37ae02511522ee2a3d2a42d14427f
danilo neves cruz <cruzdanilo@gmail.com>
2022-12-26 20:33:51 -0500

    ♻️ web3: replace legacy connect

--- hooks/useWeb3.ts
+++ hooks/useWeb3.ts
@@ -1,12 +1,9 @@
-import { InjectedConnector } from 'wagmi/connectors/injected';
-import { useAccount, useConnect, useDisconnect, useNetwork } from 'wagmi';
+import { useAccount, useNetwork } from 'wagmi';
 import { useRouter } from 'next/router';
 import * as chains from 'wagmi/chains';
 import useDebounce from './useDebounce';
 
 export const useWeb3 = () => {
-  const { connect } = useConnect({ connector: new InjectedConnector() });
-  const { disconnect } = useDisconnect();
   const { address, isConnected } = useAccount();
   const { chain } = useNetwork();
   const { query } = useRouter();
@@ -18,7 +15,5 @@ export const useWeb3 = () => {
     chain: walletAddress
       ? chain
       : chains[(typeof query.n === 'string' ? query.n : process.env.NEXT_PUBLIC_NETWORK) as keyof typeof chains],
-    connect,
-    disconnect,
   };
 };
