2be33604d3b57e91a653f7f7e1d6322ee321e035
danilo neves cruz <cruzdanilo@gmail.com>
2022-12-26 21:29:50 -0500

    👔 remove unsupported chains

--- utils/client.ts
+++ utils/client.ts
@@ -1,21 +1,16 @@
 import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum';
-import { createClient, configureChains, Chain } from 'wagmi';
-import { mainnet, optimism, goerli, optimismGoerli } from 'wagmi/chains';
+import { createClient, configureChains } from 'wagmi';
+import { mainnet, goerli } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
 export const walletConnectId = '11ddaa8aaede72cb5d6b0dae2fed7baa';
 
 const { NEXT_PUBLIC_ENABLE_TESTNETS = 'false' } = process.env;
 
-export const mainnetChains: Chain[] = [mainnet, optimism];
-export const testnetChains: Chain[] = [goerli, optimismGoerli];
-export const enableTestnets: boolean = JSON.parse(NEXT_PUBLIC_ENABLE_TESTNETS);
-export const allowedChains: Chain[] = [...mainnetChains, ...(enableTestnets ? [...testnetChains] : [])];
-
-const { chains, provider } = configureChains(allowedChains, [
-  publicProvider(),
-  walletConnectProvider({ projectId: walletConnectId }),
-]);
+const { chains, provider } = configureChains(
+  [mainnet, ...(JSON.parse(NEXT_PUBLIC_ENABLE_TESTNETS) ? [goerli] : [])],
+  [publicProvider(), walletConnectProvider({ projectId: walletConnectId })],
+);
 
 export const wagmi = createClient({
   autoConnect: true,
