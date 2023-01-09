9a0c626e0d9a8e8d1f4ca0c301c903b1c6483907
Jorge Galat <jrgglt@gmail.com>
2023-01-09 16:19:13 -0300

    🩹 use ts instead of prop-types

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import type { NextPage } from 'next';
 import * as Sentry from '@sentry/nextjs';
 import NextErrorComponent from 'next/error';
 
-const CustomErrorComponent: NextPage<{ statusCode: number }> = ({ statusCode }) => (
-  <NextErrorComponent statusCode={statusCode} />
-);
+type Props = {
+  statusCode: number;
+};
+
+const CustomErrorComponent: NextPage<Props> = ({ statusCode }: Props) => <NextErrorComponent statusCode={statusCode} />;
 
 CustomErrorComponent.getInitialProps = async (contextData) => {
   // In case this is running in a serverless function, await this in order to give Sentry
@@ -15,10 +16,6 @@ CustomErrorComponent.getInitialProps = async (contextData) => {
 
   // This will contain the status code of the response
   return NextErrorComponent.getInitialProps(contextData);
-};
-
-CustomErrorComponent.propTypes = {
-  statusCode: PropTypes.number.isRequired,
 };
 
 export default CustomErrorComponent;
