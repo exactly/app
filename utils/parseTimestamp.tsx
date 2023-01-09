a38b7cc79c8610a0587c1a10d3ba308d8167c698
Juan Pablo Rombola <juampi.rombola@gmail.com>
2023-01-09 15:26:09 -0300

    🚸 show maturity under best borrow/deposit aprs

--- utils/parseTimestamp.tsx
+++ utils/parseTimestamp.tsx
@@ -1,8 +1,8 @@
 import dayjs from 'dayjs';
 
-export default function parseTimestamp(timestamp: string | number) {
+export default function parseTimestamp(timestamp: string | number, format = 'MMM DD, YYYY') {
   if (typeof timestamp === 'string') {
     timestamp = parseInt(timestamp);
   }
-  return dayjs.unix(timestamp).format('MMM DD, YYYY');
+  return dayjs.unix(timestamp).format(format);
 }
