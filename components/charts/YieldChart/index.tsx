f147fa3f04195d998484e57bdfd82b8da68da733
franm <fran.marienhoff@gmail.com>
2023-01-10 17:32:36 -0300

    🎨 add loading and buttons selectors

--- components/charts/YieldChart/index.tsx
+++ components/charts/YieldChart/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from '@mui/material';
-import React, { FC } from 'react';
+import React, { FC, useMemo, useState } from 'react';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import updateLocale from 'dayjs/plugin/updateLocale';
@@ -8,6 +8,8 @@ import useAssets from 'hooks/useAssets';
 import useYieldRates from 'hooks/useYieldRates';
 import parseTimestamp from 'utils/parseTimestamp';
 import { toPercentage } from 'utils/utils';
+import ButtonsChart from '../ButtonsChart';
+import LoadingChart from '../LoadingChart';
 
 type Props = {
   symbol?: string;
@@ -49,41 +51,62 @@ const referenceLabel = (t: number) => {
 };
 
 const YieldChart: FC<Props> = () => {
-  const { depositsRates } = useYieldRates();
-
+  const { depositsRates, borrowsRates, loading } = useYieldRates();
+  const [operation, setOperation] = useState<'Deposits' | 'Borrows'>('Deposits');
   const assets = useAssets();
+
+  const buttons = useMemo(
+    () => [
+      {
+        label: 'Deposits',
+        onClick: () => setOperation('Deposits'),
+      },
+      {
+        label: 'Borrows',
+        onClick: () => setOperation('Borrows'),
+      },
+    ],
+    [],
+  );
 
   return (
     <Box display="flex" flexDirection="column" width="100%" height="100%" gap={2}>
       <Box display="flex" justifyContent="space-between">
         <Typography variant="h6">Yield Curve</Typography>
+        <Box>
+          <ButtonsChart buttons={buttons} />
+        </Box>
       </Box>
 
       <ResponsiveContainer width="100%" height="100%">
-        <AreaChart data={depositsRates}>
-          <XAxis
-            dataKey="maturity"
-            type="number"
-            stroke="#8f8c9c"
-            tickMargin={16}
-            tickFormatter={(t) => formatXAxis(t)}
-            domain={['dataMin', 'dataMax']}
-            scale="time"
-          />
-          <YAxis tickFormatter={(value) => toPercentage(value as number)} type="number" tickLine={false} />
-          <Tooltip
-            formatter={(value) => toPercentage(value as number)}
-            labelFormatter={(value) => {
-              return `${parseTimestamp(value)}`;
-            }}
-          />
-          {assets.map((asset) => (
-            <Area key={asset} type="monotone" dataKey={asset} stroke={getRandomColor()} fillOpacity={0} />
-          ))}
-          {getReferenceLines().map((reference) => (
-            <ReferenceLine key={reference} x={reference} label={referenceLabel(reference)} />
-          ))}
-        </AreaChart>
+        {loading ? (
+          <LoadingChart />
+        ) : (
+          <AreaChart data={operation === 'Deposits' ? depositsRates : borrowsRates}>
+            <XAxis
+              dataKey="maturity"
+              type="number"
+              stroke="#8f8c9c"
+              tickMargin={16}
+              tickFormatter={(t) => formatXAxis(t)}
+              domain={['dataMin', 'dataMax']}
+              scale="time"
+            />
+            <YAxis tickFormatter={(value) => toPercentage(value as number)} type="number" tickLine={false} />
+            <Tooltip
+              formatter={(value) => toPercentage(value as number)}
+              labelFormatter={(value) => {
+                return `${parseTimestamp(value)}`;
+              }}
+            />
+            {assets.map((asset) => (
+              <Area key={asset} type="monotone" dataKey={asset} stroke={getRandomColor()} fillOpacity={0} />
+            ))}
+            {getReferenceLines().map((reference) => (
+              <ReferenceLine key={reference} x={reference} label={referenceLabel(reference)} />
+            ))}
+          </AreaChart>
+        )}
       </ResponsiveContainer>
     </Box>
   );
