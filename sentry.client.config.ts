5d9e4ece3cf232a7ae05d36762dd04d2fe002254
Jorge Galat <jrgglt@gmail.com>
2023-01-13 11:58:40 -0300

    🐛use `NEXT_PUBLIC_` prefix on sentry env

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,10 +2,11 @@ import * as Sentry from '@sentry/nextjs';
 import { ExtraErrorData } from '@sentry/integrations';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_ENVIRONMENT = process.env.SENTRY_ENVIRONMENT || process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT;
 
 Sentry.init({
   dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
   integrations: [new ExtraErrorData({ depth: 5 })],
-  environment: process.env.SENTRY_ENVIRONMENT,
+  environment: SENTRY_ENVIRONMENT,
 });
