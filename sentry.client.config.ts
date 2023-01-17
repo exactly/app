b33d61f4e7781736a810ff670ef6d002769ca122
danilo neves cruz <cruzdanilo@gmail.com>
2023-01-16 22:07:23 -0500

    ✨ sentry: enable error replays

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,12 +1,13 @@
-import * as Sentry from '@sentry/nextjs';
+import { init, Replay } from '@sentry/nextjs';
 import { ExtraErrorData } from '@sentry/integrations';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 const SENTRY_ENVIRONMENT = process.env.SENTRY_ENVIRONMENT || process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT;
 
-Sentry.init({
+init({
   dsn: SENTRY_DSN,
-  tracesSampleRate: 1.0,
-  integrations: [new ExtraErrorData({ depth: 5 })],
   environment: SENTRY_ENVIRONMENT,
+  tracesSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [new ExtraErrorData({ depth: 5 }), Replay && new Replay()].filter(Boolean),
 });
