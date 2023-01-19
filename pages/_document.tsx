17151c3395c8c3dd8adaa1db158dd562d3888e06
danilo neves cruz <cruzdanilo@gmail.com>
2023-01-19 10:18:31 -0500

    🚨 fix fonts cross origin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
     <Html lang="en-US">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
