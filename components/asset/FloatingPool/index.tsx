94eb68faaed876827db143dd96d2073d43ce5d06
Juan Pablo Rombola <juampi.rombola@gmail.com>
2023-01-23 15:42:26 -0300

    💄 change asset page layout and update styles

--- components/asset/FloatingPool/index.tsx
+++ components/asset/FloatingPool/index.tsx
@@ -2,7 +2,11 @@ import React, { FC } from 'react';
 import Grid from '@mui/material/Grid';
 
 import FloatingPoolInfo from './FloatingPoolInfo';
-// import FloatingAPRChart from './FloatingAPRChart';
+import { Box } from '@mui/material';
+import HistoricalRateChart from 'components/charts/HistoricalRateChart';
+import { globals } from 'styles/theme';
+
+const { onlyDesktop } = globals;
 
 type AssetFloatingPoolProps = {
   symbol: string;
@@ -11,17 +15,30 @@ type AssetFloatingPoolProps = {
 
 const AssetFloatingPool: FC<AssetFloatingPoolProps> = ({ symbol, eMarketAddress }) => {
   return (
-    <Grid
-      item
-      xs={12}
-      width={'100%'}
-      boxShadow="0px 4px 12px rgba(175, 177, 182, 0.2)"
-      borderRadius="0px 0px 6px 6px"
-      bgcolor="white"
-      borderTop="4px solid #33CC59"
-    >
-      <FloatingPoolInfo symbol={symbol} eMarketAddress={eMarketAddress} />
-    </Grid>
+    <Box display="flex" flexDirection="column" gap="8px">
+      <Grid
+        item
+        xs={12}
+        width="100%"
+        boxShadow="0px 4px 12px rgba(175, 177, 182, 0.2)"
+        borderRadius="0px 0px 6px 6px"
+        bgcolor="white"
+        borderTop="4px solid #33CC59"
+      >
+        <FloatingPoolInfo symbol={symbol} eMarketAddress={eMarketAddress} />
+      </Grid>
+      <Box
+        boxShadow="0px 4px 12px rgba(175, 177, 182, 0.2)"
+        borderRadius="0px 0px 6px 6px"
+        bgcolor="white"
+        p="16px"
+        display={onlyDesktop} // TODO: are we going to have it on mobile?
+        width={610}
+        height={350}
+      >
+        <HistoricalRateChart symbol={symbol} />
+      </Box>
+    </Box>
   );
 };
 
