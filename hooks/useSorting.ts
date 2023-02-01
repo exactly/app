951bb2aab8c7537250b7ca3450a0474f2aaf016b
Juan Pablo Rombola <juampi.rombola@gmail.com>
2023-02-01 10:32:10 -0300

    ✨ add useSorting hook

---
+++ hooks/useSorting.ts
@@ -0,0 +1,41 @@
+import { useCallback, useState } from 'react';
+
+type Sort<T> = {
+  key?: keyof T;
+  direction?: 'asc' | 'desc';
+};
+
+export default <T>() => {
+  const [sort, setSort] = useState<Sort<T>>({});
+  const isActive = useCallback((key: keyof T) => Boolean(sort.key === key && sort.direction), [sort]);
+  const direction = useCallback(
+    (key: keyof T) => (sort.key === key ? sort.direction : undefined),
+    [sort.key, sort.direction],
+  );
+
+  const setOrderBy = (key?: keyof T) => {
+    if (!key) return;
+    sort.key === key
+      ? setSort({ key, direction: sort.direction === 'asc' ? 'desc' : undefined })
+      : setSort({ key, direction: 'asc' });
+  };
+
+  const sortData = (data: T[]) => {
+    if (!sort.key || !sort.direction) {
+      return data;
+    }
+
+    const currentKey = sort.key as keyof T;
+    return [...data].sort((a, b) => {
+      if (a[currentKey] < b[currentKey]) {
+        return sort.direction === 'asc' ? -1 : 1;
+      }
+      if (a[currentKey] > b[currentKey]) {
+        return sort.direction === 'asc' ? 1 : -1;
+      }
+      return 0;
+    });
+  };
+
+  return { setOrderBy, sortData, direction, isActive };
+};
