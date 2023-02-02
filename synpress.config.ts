6a95fc75b2a4dc67971900b3a2b6982644a0e008
Jorge Galat <jrgglt@gmail.com>
2023-02-02 13:38:49 -0300

    🔧 disable video recording for individual tests

---
+++ synpress.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'cypress';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import setupNodeEvents from '@synthetixio/synpress/plugins/index';
+
+export default defineConfig({
+  userAgent: 'synpress',
+  video: false,
+  screenshotsFolder: 'tests/e2e/screenshots',
+  videosFolder: 'tests/e2e/videos',
+  chromeWebSecurity: true,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  env: {
+    coverage: false,
+  },
+  defaultCommandTimeout: 30000,
+  pageLoadTimeout: 30000,
+  requestTimeout: 30000,
+  e2e: {
+    setupNodeEvents,
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'tests/e2e/specs/**/*.ts',
+    supportFile: 'tests/e2e/support.ts',
+  },
+});
