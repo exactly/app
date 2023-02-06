b8a3d91123ea4373a9bf73e6edc03deb7e3fa966
Juan Pablo Rombola <juampi.rombola@gmail.com>
2023-02-06 10:30:06 -0300

    🚧 add MarketsBasic component w/mock data

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,23 @@ import React, { useEffect } from 'react';
 import type { NextPage } from 'next';
 import Grid from '@mui/material/Grid';
 
-import MarketsHeader from 'components/markets/Header';
-import MarketTables from 'components/markets/MarketsTables';
+// import MarketsHeader from 'components/markets/Header';
+// import MarketTables from 'components/markets/MarketsTables';
+import MarketsBasic from 'components/markets/MarketsBasic';
 
 import analytics from 'utils/analytics';
+import { Box } from '@mui/material';
 
 const Markets: NextPage = () => {
   useEffect(() => void analytics.page(), []);
 
   return (
     <Grid>
-      <MarketsHeader />
-      <MarketTables />
+      {/* <MarketsHeader />
+      <MarketTables /> */}
+      <Box display="flex" justifyContent="center" mb={2}>
+        <MarketsBasic />
+      </Box>
     </Grid>
   );
 };
