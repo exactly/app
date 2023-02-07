eb8c64821a31875ee5892b5c6044e479b3c8a95a
Juan Pablo Rombola <juampi.rombola@gmail.com>
2023-02-07 16:55:37 -0300

    💄 adjust markets basic view styles

--- components/markets/MarketsBasic/index.tsx
+++ components/markets/MarketsBasic/index.tsx
@@ -1,10 +1,11 @@
 import { Box, Button, Divider, FormControlLabel, Radio, RadioGroup, Typography } from '@mui/material';
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import AssetInput from 'components/OperationsModal/AssetInput';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import LockIcon from '@mui/icons-material/Lock';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import SwapVertIcon from '@mui/icons-material/SwapVert';
 import Image from 'next/image';
 import useMaturityPools from 'hooks/useMaturityPools';
 import numbers from 'config/numbers.json';
@@ -12,16 +13,32 @@ import { toPercentage } from 'utils/utils';
 import daysLeft from 'utils/daysLeft';
 import useFloatingPoolAPR from 'hooks/useFloatingPoolAPR';
 import OperationTabs from './OperationTabs';
-import { useMarketsBasic } from 'contexts/MarketsBasicContext';
+import { MarketsBasicOptions, useMarketsBasic } from 'contexts/MarketsBasicContext';
+import { useOperationContext } from 'contexts/OperationContext';
+import useBalance from 'hooks/useBalance';
+import useAccountData from 'hooks/useAccountData';
+import Overview from './Overview';
 
 const MarketsBasic: FC = () => {
-  const { symbol = 'DAI', operation } = useMarketsBasic();
+  const { symbol = 'DAI', operation, selected, setSelected } = useMarketsBasic();
+  const { setErrorData, qty, setQty, assetContract } = useOperationContext();
+  const { decimals = 18 } = useAccountData(symbol);
+  const walletBalance = useBalance(symbol, assetContract);
+
+  const onMax = useCallback(() => {
+    if (walletBalance) {
+      setQty(walletBalance);
+      setErrorData(undefined);
+    }
+  }, [walletBalance, setQty, setErrorData]);
+
+  const handleInputChange = useCallback((value: string) => setQty(value), [setQty]);
 
   const maturityPools = useMaturityPools(symbol);
   const { depositAPR: floatingDepositAPR, borrowAPR: floatingBorrowAPR } = useFloatingPoolAPR(symbol);
 
   const { minAPRValue } = numbers;
-  const allPools: { maturity?: number; depositAPR?: number; borrowAPR?: number }[] = [
+  const allPools: MarketsBasicOptions[] = [
     { depositAPR: floatingDepositAPR, borrowAPR: floatingBorrowAPR },
     ...maturityPools.map(({ maturity, depositAPR, borrowAPR }) => ({ maturity, depositAPR, borrowAPR })),
   ];
@@ -35,7 +52,6 @@ const MarketsBasic: FC = () => {
         gap={1}
         bgcolor="grey.100"
         maxWidth="400px"
-        minHeight="600px"
         sx={{ borderRadius: '16px' }}
         boxShadow="4px 8px 16px rgba(227, 229, 232, 0.5), -4px -8px 16px rgba(248, 249, 249, 0.25)"
       >
@@ -43,45 +59,33 @@ const MarketsBasic: FC = () => {
 
         <Box display="flex" flexDirection="column" bgcolor="white" border="1px solid #EDF0F2" borderRadius="8px">
           <Box px={2} py={1.5}>
-            <Typography fontWeight={600} fontSize={13} color="figma.grey.600">
-              Amount
-            </Typography>
+            <Typography variant="cardTitle">Asset to {operation}</Typography>
             <AssetInput
-              qty={'0'}
-              decimals={18}
+              qty={qty}
               symbol={symbol}
-              onMax={undefined}
-              onChange={() => 1}
+              decimals={decimals}
+              onMax={onMax}
+              onChange={handleInputChange}
               label="Wallet balance"
-              amount={'100'}
+              amount={walletBalance}
             />
           </Box>
           <Divider sx={{ background: 'figma.grey.700' }} />
           <Box px={2} py={1.5}>
-            <Typography fontWeight={600} fontSize={13} color="figma.grey.600">
-              Days to maturity
-            </Typography>
-            <RadioGroup value={1} onChange={undefined} sx={{ pt: 1 }}>
+            <Typography variant="cardTitle">Days to maturity</Typography>
+            <RadioGroup value={selected} onChange={(e) => setSelected(parseInt(e.target.value))} sx={{ pt: 1 }}>
               {allPools.map(({ maturity, depositAPR, borrowAPR }) => (
                 <FormControlLabel
                   key={`${maturity}_${depositAPR}_${borrowAPR}`}
-                  value="female"
+                  value={maturity || 0}
                   control={<Radio />}
                   componentsProps={{ typography: { width: '100%' } }}
                   sx={{ m: 0, ':hover': { backgroundColor: 'grey.50' } }}
                   label={
                     <Box display="flex" flexDirection="row" py="7px" justifyContent="space-between" width="100%">
-                      <Box display="flex" flexDirection="column">
-                        <Typography fontWeight={700} fontSize={14} color="grey.900">
-                          {maturity ? daysLeft(maturity) : 'Flexible'}
-                        </Typography>
-                        <Box display="flex" gap={0.3}>
-                          <Typography fontWeight={500} fontSize={13} color="figma.grey.500">
-                            {maturity ? 'Fixed rate' : 'Variable rate'}
-                          </Typography>
-                          <InfoOutlinedIcon sx={{ fontSize: '11px', my: 'auto', color: 'figma.grey.500' }} />
-                        </Box>
-                      </Box>
+                      <Typography fontWeight={700} fontSize={14} color="grey.900" my="auto">
+                        {maturity ? daysLeft(maturity) : 'Flexible'}
+                      </Typography>
                       <Box display="flex" flexDirection="column">
                         <Box display="flex" gap={0.3} justifyContent="right">
                           <Typography fontWeight={700} fontSize={14} color="grey.900" textAlign="right">
@@ -93,15 +97,19 @@ const MarketsBasic: FC = () => {
                           <Image
                             src={`/img/assets/${symbol}.svg`}
                             alt={symbol}
-                            width="12"
-                            height="12"
+                            width="14"
+                            height="14"
                             style={{ maxWidth: '100%', height: 'auto' }}
                           />
                         </Box>
                         <Box display="flex" gap={0.3} justifyContent="right">
-                          <LockIcon sx={{ fontSize: '11px', my: 'auto', color: 'figma.grey.500' }} />
+                          {maturity ? (
+                            <LockIcon sx={{ fontSize: '11px', my: 'auto', color: 'figma.grey.500' }} />
+                          ) : (
+                            <SwapVertIcon sx={{ fontSize: '11px', my: 'auto', color: 'figma.grey.500' }} />
+                          )}
                           <Typography fontWeight={500} fontSize={13} color="figma.grey.500" textAlign="right">
-                            APR
+                            {maturity ? 'Fixed' : 'Variable'} APR
                           </Typography>
                           <InfoOutlinedIcon sx={{ fontSize: '11px', my: 'auto', color: 'figma.grey.500' }} />
                         </Box>
@@ -114,76 +122,11 @@ const MarketsBasic: FC = () => {
           </Box>
         </Box>
 
-        <Box
-          display="flex"
-          flexDirection="column"
-          p={2}
-          bgcolor="grey.100"
-          border="1px solid #E3E5E8"
-          borderRadius="8px"
-          gap={0.2}
-        >
-          <Typography fontWeight={600} fontSize={13} color="figma.grey.600">
-            Your total debt
-          </Typography>
-          <Box display="flex" gap={0.5} mb={1}>
-            <Image
-              src={`/img/assets/${symbol}.svg`}
-              alt={symbol}
-              width="20"
-              height="20"
-              style={{ maxWidth: '100%', height: 'auto' }}
-            />
-            <Typography fontWeight={700} fontSize={24}>
-              4332.25
-            </Typography>
-          </Box>
-          <Box display="flex" justifyContent="space-between">
-            <Typography fontWeight={500} fontSize={13} color="figma.grey.500">
-              Total Principal
-            </Typography>
-            <Box display="flex" gap={0.3}>
-              <Typography fontWeight={700} fontSize={13}>
-                4300
-              </Typography>
-              <Image
-                src={`/img/assets/${symbol}.svg`}
-                alt={symbol}
-                width="12"
-                height="12"
-                style={{ maxWidth: '100%', height: 'auto' }}
-              />
-            </Box>
-          </Box>
-          <Box display="flex" justifyContent="space-between">
-            <Typography fontWeight={500} fontSize={13} color="figma.grey.500">
-              Total interest (+0.75%)
-            </Typography>
-            <Box display="flex" gap={0.3}>
-              <Typography fontWeight={700} fontSize={13}>
-                32.25
-              </Typography>
-              <Image
-                src={`/img/assets/${symbol}.svg`}
-                alt={symbol}
-                width="12"
-                height="12"
-                style={{ maxWidth: '100%', height: 'auto' }}
-              />
-            </Box>
-          </Box>
-          <Box display="flex" justifyContent="space-between">
-            <Typography fontWeight={500} fontSize={13} color="figma.grey.500">
-              Maturity date (In 88 days)
-            </Typography>
-            <Typography fontWeight={700} fontSize={13}>
-              June 15th, 2023
-            </Typography>
-          </Box>
-        </Box>
+        {Boolean(selected) && Boolean(qty) && <Overview symbol={symbol} />}
+
         <Box mt={1}>
           <Button fullWidth variant="contained">
-            Borrow USDC
+            {`${operation === 'deposit' ? 'Deposit' : 'Borrow'} ${symbol}`}
           </Button>
         </Box>
       </Box>
