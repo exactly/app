434f8a0cd93af4b2f34111d9cfff8bd4794922f6
Juan Pablo Rombola <juampi.rombola@gmail.com>
2023-02-09 11:53:17 -0300

    ♻️  extract deposit logic to useDeposit hook

---
+++ hooks/useDeposit.ts
@@ -0,0 +1,190 @@
+import { BigNumber, formatFixed, parseFixed } from '@ethersproject/bignumber';
+import { WeiPerEther } from '@ethersproject/constants';
+import numbers from 'config/numbers.json';
+import AccountDataContext from 'contexts/AccountDataContext';
+import { useOperationContext, usePreviewTx } from 'contexts/OperationContext';
+import useAccountData from 'hooks/useAccountData';
+import useApprove from 'hooks/useApprove';
+import useBalance from 'hooks/useBalance';
+import useHandleOperationError from 'hooks/useHandleOperationError';
+import { useWeb3 } from 'hooks/useWeb3';
+import { useCallback, useContext, useMemo } from 'react';
+import analytics from 'utils/analytics';
+
+const DEFAULT_AMOUNT = BigNumber.from(numbers.defaultAmount);
+
+type Deposit = {
+  isLoading: boolean;
+  onMax: () => void;
+  handleInputChange: (value: string) => void;
+  handleSubmitAction: () => void;
+  deposit: () => void;
+};
+
+export default (): Deposit => {
+  const { walletAddress } = useWeb3();
+  const { getAccountData } = useContext(AccountDataContext);
+
+  const {
+    symbol,
+    setErrorData,
+    qty,
+    setQty,
+    setTx,
+    requiresApproval,
+    setRequiresApproval,
+    isLoading: isLoadingOp,
+    setIsLoading: setIsLoadingOp,
+    marketContract,
+    assetContract,
+    ETHRouterContract,
+  } = useOperationContext();
+
+  const handleOperationError = useHandleOperationError();
+
+  const { decimals = 18 } = useAccountData(symbol);
+
+  const walletBalance = useBalance(symbol, assetContract);
+
+  const {
+    approve,
+    estimateGas: approveEstimateGas,
+    isLoading: approveIsLoading,
+    needsApproval,
+  } = useApprove('deposit', assetContract, marketContract?.address);
+
+  const previewGasCost = useCallback(
+    async (quantity: string): Promise<BigNumber | undefined> => {
+      if (!walletAddress || !ETHRouterContract || !marketContract || !quantity) return;
+
+      const gasPrice = (await ETHRouterContract.provider.getFeeData()).maxFeePerGas;
+      if (!gasPrice) return;
+
+      if (requiresApproval) {
+        const gasEstimation = await approveEstimateGas();
+        return gasEstimation?.mul(gasPrice);
+      }
+
+      if (symbol === 'WETH') {
+        const gasLimit = await ETHRouterContract.estimateGas.deposit({
+          value: quantity ? parseFixed(quantity, 18) : DEFAULT_AMOUNT,
+        });
+
+        return gasPrice.mul(gasLimit);
+      }
+
+      const gasLimit = await marketContract.estimateGas.deposit(
+        quantity ? parseFixed(quantity, decimals) : DEFAULT_AMOUNT,
+        walletAddress,
+      );
+
+      return gasPrice.mul(gasLimit);
+    },
+    [decimals, ETHRouterContract, approveEstimateGas, marketContract, requiresApproval, symbol, walletAddress],
+  );
+
+  const { isLoading: previewIsLoading } = usePreviewTx({ qty, needsApproval, previewGasCost });
+
+  const isLoading = useMemo(
+    () => approveIsLoading || isLoadingOp || previewIsLoading,
+    [approveIsLoading, isLoadingOp, previewIsLoading],
+  );
+
+  const onMax = useCallback(() => {
+    if (walletBalance) {
+      setQty(walletBalance);
+      setErrorData(undefined);
+    }
+  }, [walletBalance, setQty, setErrorData]);
+
+  const handleInputChange = useCallback(
+    (value: string) => {
+      setQty(value);
+
+      if (walletBalance && parseFloat(value) > parseFloat(walletBalance)) {
+        return setErrorData({
+          status: true,
+          message: `You can't deposit more than you have in your wallet`,
+          component: 'input',
+        });
+      }
+
+      setErrorData(undefined);
+    },
+    [setQty, walletBalance, setErrorData],
+  );
+
+  const deposit = useCallback(async () => {
+    if (!walletAddress || !marketContract) return;
+    let depositTx;
+    try {
+      setIsLoadingOp(true);
+      if (symbol === 'WETH') {
+        if (!ETHRouterContract) return;
+
+        const gasEstimation = await ETHRouterContract.estimateGas.deposit({ value: parseFixed(qty, 18) });
+
+        depositTx = await ETHRouterContract.deposit({
+          value: parseFixed(qty, 18),
+          gasLimit: gasEstimation.mul(parseFixed(String(numbers.gasLimitMultiplier), 18)).div(WeiPerEther),
+        });
+      } else {
+        const depositAmount = parseFixed(qty, decimals);
+        const gasEstimation = await marketContract.estimateGas.deposit(depositAmount, walletAddress);
+
+        depositTx = await marketContract.deposit(depositAmount, walletAddress, {
+          gasLimit: Math.ceil(Number(formatFixed(gasEstimation)) * numbers.gasLimitMultiplier),
+        });
+      }
+
+      setTx({ status: 'processing', hash: depositTx.hash });
+
+      const { status, transactionHash } = await depositTx.wait();
+
+      setTx({ status: status ? 'success' : 'error', hash: transactionHash });
+
+      void analytics.track(status ? 'deposit' : 'depositRevert', {
+        amount: qty,
+        asset: symbol,
+        hash: transactionHash,
+      });
+
+      void getAccountData();
+    } catch (error) {
+      if (depositTx) setTx({ status: 'error', hash: depositTx.hash });
+      setErrorData({ status: true, message: handleOperationError(error) });
+    } finally {
+      setIsLoadingOp(false);
+    }
+  }, [
+    walletAddress,
+    marketContract,
+    setIsLoadingOp,
+    symbol,
+    setTx,
+    qty,
+    getAccountData,
+    ETHRouterContract,
+    decimals,
+    setErrorData,
+    handleOperationError,
+  ]);
+
+  const handleSubmitAction = useCallback(async () => {
+    if (isLoading) return;
+    if (requiresApproval) {
+      await approve();
+      setRequiresApproval(await needsApproval(qty));
+      return;
+    }
+
+    void analytics.track('depositRequest', {
+      amount: qty,
+      asset: symbol,
+    });
+
+    return deposit();
+  }, [isLoading, requiresApproval, qty, symbol, deposit, approve, setRequiresApproval, needsApproval]);
+
+  return { isLoading, onMax, handleInputChange, handleSubmitAction, deposit };
+};
