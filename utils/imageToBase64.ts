0cec6be6675c9f37b0c3bc3c053c1743bb1be2a4
franm <fran.marienhoff@gmail.com>
2023-02-10 16:05:46 -0300

    🚸 add base64 tokens

---
+++ utils/imageToBase64.ts
@@ -0,0 +1,16 @@
+export default (url: string) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(xhr.response);
+    };
+    xhr.onerror = reject;
+    xhr.open('GET', url);
+    xhr.responseType = 'blob';
+    xhr.send();
+  });
+};
