bc3a8ca5fa111d62a4962b48b6d7d7560edcb0b0
franm <fran.marienhoff@gmail.com>
2023-02-10 17:32:26 -0300

    🐛 add auditor for optimism goerli

--- hooks/useAuditor.ts
+++ hooks/useAuditor.ts
@@ -1,9 +1,11 @@
 import { useMemo } from 'react';
 import { Contract } from '@ethersproject/contracts';
-import { useSigner, mainnet, goerli } from 'wagmi';
+import { useSigner } from 'wagmi';
+import { mainnet, goerli, optimismGoerli } from 'wagmi/chains';
 import type { Auditor } from 'types/contracts/Auditor';
 import mainnetAuditor from '@exactly-protocol/protocol/deployments/mainnet/Auditor.json' assert { type: 'json' };
 import goerliAuditor from '@exactly-protocol/protocol/deployments/goerli/Auditor.json' assert { type: 'json' };
+import optimismGoerliAuditor from '@exactly-protocol/protocol/deployments/optimism-goerli/Auditor.json' assert { type: 'json' };
 import auditorABI from 'abi/Auditor.json' assert { type: 'json' };
 import { useWeb3 } from './useWeb3';
 
@@ -17,6 +19,7 @@ export default () => {
     const address = {
       [goerli.id]: goerliAuditor.address,
       [mainnet.id]: mainnetAuditor.address,
+      [optimismGoerli.id]: optimismGoerliAuditor.address,
     }[chain.id];
     if (!address) return null;
 
