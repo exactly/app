436568418278c6832c7856f05335e667b875e2bc
Juan Pablo Rombola <juampi.rombola@gmail.com>
2023-02-14 09:41:44 -0300

    🚸 filter fixed pools with 0 apr

--- components/markets/MarketsBasic/Options/index.tsx
+++ components/markets/MarketsBasic/Options/index.tsx
@@ -34,84 +34,87 @@ const Options: FC<Props> = ({
 
   return (
     <RadioGroup value={selected} onChange={(e) => setSelected(parseInt(e.target.value))} sx={{ pt: 1 }}>
-      {allOptions.map(({ maturity, depositAPR, borrowAPR }, index) => (
-        <FormControlLabel
-          key={`${maturity}_${depositAPR}_${borrowAPR}_${index}`}
-          value={maturity}
-          control={<Radio />}
-          componentsProps={{ typography: { width: '100%' } }}
-          sx={{ m: 0, ':hover': { backgroundColor: 'grey.50' } }}
-          disabled={maturity !== 0 && !maturity}
-          label={
-            <Box
-              display="flex"
-              flexDirection="row"
-              py="7px"
-              justifyContent="space-between"
-              alignItems="center"
-              width="100%"
-            >
-              <Box display="flex" gap={0.5} alignItems="center">
-                {maturity || maturity === 0 ? (
-                  <Typography fontWeight={700} fontSize={14} color="grey.900" my="auto">
-                    {maturity ? daysLeft(maturity) : 'Flexible'}
-                  </Typography>
-                ) : (
-                  <Skeleton width={52} height={20} />
-                )}
-                {bestOption === maturity && (
-                  <Box
-                    display="flex"
-                    alignItems="center"
-                    height="16px"
-                    py="3px"
-                    px="6px"
-                    borderRadius="8px"
-                    sx={{ background: 'linear-gradient(66.92deg, #00CC68 34.28%, #00CC8F 100%)' }}
-                  >
-                    <Typography variant="chip" color="white">
-                      BEST
-                    </Typography>
+      {allOptions.map(
+        ({ maturity, depositAPR, borrowAPR }, index) =>
+          (operation === 'borrow' || Boolean(depositAPR && depositAPR >= minAPRValue)) && (
+            <FormControlLabel
+              key={`${maturity}_${depositAPR}_${borrowAPR}_${index}`}
+              value={maturity}
+              control={<Radio />}
+              componentsProps={{ typography: { width: '100%' } }}
+              sx={{ m: 0, ':hover': { backgroundColor: 'grey.50' } }}
+              disabled={maturity !== 0 && !maturity}
+              label={
+                <Box
+                  display="flex"
+                  flexDirection="row"
+                  py="7px"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  width="100%"
+                >
+                  <Box display="flex" gap={0.5} alignItems="center">
+                    {maturity || maturity === 0 ? (
+                      <Typography fontWeight={700} fontSize={14} color="grey.900" my="auto">
+                        {maturity ? daysLeft(maturity) : 'Flexible'}
+                      </Typography>
+                    ) : (
+                      <Skeleton width={52} height={20} />
+                    )}
+                    {bestOption === maturity && (
+                      <Box
+                        display="flex"
+                        alignItems="center"
+                        height="16px"
+                        py="3px"
+                        px="6px"
+                        borderRadius="8px"
+                        sx={{ background: 'linear-gradient(66.92deg, #00CC68 34.28%, #00CC8F 100%)' }}
+                      >
+                        <Typography variant="chip" color="white">
+                          BEST
+                        </Typography>
+                      </Box>
+                    )}
                   </Box>
-                )}
-              </Box>
-              <Box display="flex" flexDirection="column">
-                <Box display="flex" gap={0.3} justifyContent="right">
-                  {(maturity === 0 ? !loadingFloatingOption : !loadingFixedOptions) ? (
-                    <Typography fontWeight={700} fontSize={14} color="grey.900" textAlign="right">
-                      {`${
-                        operation === 'deposit'
-                          ? toPercentage((depositAPR || 0) > minAPRValue ? depositAPR : undefined)
-                          : toPercentage((borrowAPR || 0) > minAPRValue ? borrowAPR : undefined)
-                      } APR`}
-                    </Typography>
-                  ) : (
-                    <Skeleton width={40} height={20} />
-                  )}
-                  <Image
-                    src={`/img/assets/${symbol}.svg`}
-                    alt={symbol}
-                    width="14"
-                    height="14"
-                    style={{ maxWidth: '100%', height: 'auto' }}
-                  />
+                  <Box display="flex" flexDirection="column">
+                    <Box display="flex" gap={0.3} justifyContent="right">
+                      {(maturity === 0 ? !loadingFloatingOption : !loadingFixedOptions) ? (
+                        <Typography fontWeight={700} fontSize={14} color="grey.900" textAlign="right">
+                          {`${
+                            operation === 'deposit'
+                              ? toPercentage((depositAPR || 0) > minAPRValue ? depositAPR : undefined)
+                              : toPercentage((borrowAPR || 0) > minAPRValue ? borrowAPR : undefined)
+                          } APR`}
+                        </Typography>
+                      ) : (
+                        <Skeleton width={40} height={20} />
+                      )}
+                      <Image
+                        src={`/img/assets/${symbol}.svg`}
+                        alt={symbol}
+                        width="14"
+                        height="14"
+                        style={{ maxWidth: '100%', height: 'auto' }}
+                      />
+                    </Box>
+                    <Box display="flex" gap={0.3} justifyContent="right">
+                      {maturity ? (
+                        <LockIcon sx={{ fontSize: '11px', my: 'auto', color: 'figma.grey.500' }} />
+                      ) : (
+                        <SwapVertIcon sx={{ fontSize: '11px', my: 'auto', color: 'figma.grey.500' }} />
+                      )}
+                      <Typography fontWeight={500} fontSize={13} color="figma.grey.500" textAlign="right">
+                        {maturity === 0 ? 'Variable' : 'Fixed'} interest rate
+                      </Typography>
+                      <InfoOutlinedIcon sx={{ fontSize: '11px', my: 'auto', color: 'figma.grey.500' }} />
+                    </Box>
+                  </Box>
                 </Box>
-                <Box display="flex" gap={0.3} justifyContent="right">
-                  {maturity ? (
-                    <LockIcon sx={{ fontSize: '11px', my: 'auto', color: 'figma.grey.500' }} />
-                  ) : (
-                    <SwapVertIcon sx={{ fontSize: '11px', my: 'auto', color: 'figma.grey.500' }} />
-                  )}
-                  <Typography fontWeight={500} fontSize={13} color="figma.grey.500" textAlign="right">
-                    {maturity === 0 ? 'Variable' : 'Fixed'} interest rate
-                  </Typography>
-                  <InfoOutlinedIcon sx={{ fontSize: '11px', my: 'auto', color: 'figma.grey.500' }} />
-                </Box>
-              </Box>
-            </Box>
-          }
-        />
-      ))}
+              }
+            />
+          ),
+      )}
     </RadioGroup>
   );
 };
