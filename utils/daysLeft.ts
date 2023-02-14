e1fdf3f2097248c08a4d0ae09ae6b0a07f353206
Juan Pablo Rombola <juampi.rombola@gmail.com>
2023-02-14 15:49:03 -0300

    🐛 fix days left

--- utils/daysLeft.ts
+++ utils/daysLeft.ts
@@ -1,5 +1,5 @@
 export default (maturity: number) => {
   const diff = maturity - Date.now() / 1000;
-  const daysLeft = Math.max(Math.ceil(diff / (3600 * 24)), 0);
+  const daysLeft = Math.max(Math.floor(diff / (3600 * 24)), 0);
   return `${daysLeft} ${daysLeft === 1 ? 'day' : 'days'}`;
 };
