4bcc29157ea36d4f546dcbc317ed1b7fbd3144c3
Jorge Galat <jrgglt@gmail.com>
2023-02-22 11:53:59 -0300

    💄 add background circle

---
+++ components/BackgroundCircle/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+export default function BackgroundCircle() {
+  return (
+    <Box
+      sx={{
+        paddingTop: '108px',
+        width: '100vh',
+        height: '100vh',
+        position: 'absolute',
+        overflowY: 'hidden',
+        top: 0,
+        transform: 'translateX(50%)',
+        zIndex: -1,
+        right: '50%',
+      }}
+    >
+      <Box
+        sx={{
+          width: '100vh',
+          height: '100vh',
+          background: 'linear-gradient(0deg, #F9FAFB 0%, rgba(249, 250, 251, 0) 100%)',
+          borderRadius: '50%',
+          opacity: 0.8,
+        }}
+      />
+    </Box>
+  );
+}
