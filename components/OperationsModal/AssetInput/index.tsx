c04ca8c8d7668bbb5045e2fece0cd4fe858cfb91
Juan Pablo Rombola <juampi.rombola@gmail.com>
2023-03-02 17:09:20 -0300

    🚸 hide wallet balance when user is not connected on simple market view

--- components/OperationsModal/AssetInput/index.tsx
+++ components/OperationsModal/AssetInput/index.tsx
@@ -5,6 +5,7 @@ import ModalInput from 'components/common/modal/ModalInput';
 import AvailableAmount, { Props as AAProps } from '../AvailableAmount';
 import USDValue from '../USDValue';
 import AssetSelector from '../AssetSelector';
+import { useWeb3 } from 'hooks/useWeb3';
 
 type Props = {
   qty: string;
@@ -15,14 +16,26 @@ type Props = {
 } & AAProps;
 
 function AssetInput({ qty, onChange, symbol, decimals, amount, label, onMax, tooltip }: Props) {
+  const { isConnected } = useWeb3();
+
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <AssetSelector />
         <ModalInput value={qty} decimals={decimals} onValueChange={onChange} />
       </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: 0.25, height: 20 }}>
-        <AvailableAmount symbol={symbol} label={label} amount={amount} onMax={onMax} tooltip={tooltip} />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: isConnected ? 'space-between' : 'right',
+          alignItems: 'center',
+          marginTop: 0.25,
+          height: 20,
+        }}
+      >
+        {isConnected && (
+          <AvailableAmount symbol={symbol} label={label} amount={amount} onMax={onMax} tooltip={tooltip} />
+        )}
         <USDValue qty={qty} symbol={symbol} />
       </Box>
     </Box>
