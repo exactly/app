599298666320ff877f8f1d3bbad1d43f37339d03
franm <fran.marienhoff@gmail.com>
2023-03-16 10:16:50 -0300

    ♻️ remove second analytics key

--- hooks/useAnalytics.ts
+++ hooks/useAnalytics.ts
@@ -1,10 +1,5 @@
-import { useMemo } from 'react';
-import { useWeb3 } from './useWeb3';
-import analytics from 'utils/analytics';
-import { mainnet } from '@wagmi/chains';
-import { AnalyticsInstance } from 'analytics';
+import { analytics } from 'utils/analytics';
 
 export default function useAnalytics() {
-  const { chain } = useWeb3();
-  return useMemo<AnalyticsInstance>(() => analytics[chain.id] || analytics[mainnet.id], [chain.id]);
+  return analytics;
 }
