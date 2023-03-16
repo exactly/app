1634a69d9e08c97bc2e74388f922b57b56025c8b
franm <fran.marienhoff@gmail.com>
2023-03-16 13:04:54 -0300

    🎨 useAnalytics improve

--- hooks/useAnalytics.ts
+++ hooks/useAnalytics.ts
@@ -6,29 +6,17 @@ import { mainnet } from 'wagmi';
 export default () => {
   const { chain } = useWeb3();
 
-  const network = useMemo(() => {
-    if (chain.id === mainnet.id) return 'mainnet';
-
-    return chain.name;
-  }, [chain.id, chain.name]);
+  const network = useMemo(() => (chain.id === mainnet.id ? 'mainnet' : chain.name), [chain.id, chain.name]);
 
   const track = useCallback(
-    (name: string, payload?: Record<string, string | number | undefined>) => {
-      analytics.track(name, { ...payload }, { network });
-    },
+    (name: string, payload?: Record<string, string | number | undefined>) =>
+      analytics.track(name, { ...payload }, { network }),
     [network],
   );
 
-  const page = useCallback(() => {
-    analytics.page(undefined, { network });
-  }, [network]);
+  const page = useCallback(() => analytics.page(undefined, { network }), [network]);
 
-  const identify = useCallback(
-    (id: string) => {
-      analytics.identify(id, undefined, { network });
-    },
-    [network],
-  );
+  const identify = useCallback((id: string) => analytics.identify(id, undefined, { network }), [network]);
 
   return { track, page, identify };
 };
