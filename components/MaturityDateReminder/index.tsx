eb5d0c70d424c5627cac83722e84403f1bec99db
franm <fran.marienhoff@gmail.com>
2023-04-11 18:11:40 -0300

    ✨ add notification when a maturity date is approaching

---
+++ components/MaturityDateReminder/index.tsx
@@ -0,0 +1,58 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import { useTranslation } from 'react-i18next';
+import { Alert, Slide, SlideProps } from '@mui/material';
+import useAccountData from 'hooks/useAccountData';
+import { BigNumber } from '@ethersproject/bignumber';
+import parseTimestamp from 'utils/parseTimestamp';
+
+export default function MaturityDateReminder() {
+  const { t } = useTranslation();
+  const { accountData } = useAccountData();
+  const [openReminder, setOpenReminder] = useState<boolean>(false);
+  const [date, setDate] = useState<string>('');
+
+  useEffect(() => {
+    if (!accountData) return;
+
+    accountData.forEach((fixedLender) => {
+      fixedLender.fixedBorrowPositions.forEach((borrowPosition) => {
+        const { maturity } = borrowPosition;
+
+        const secondsInADay = 86_400;
+        const rangeInSeconds = BigNumber.from(secondsInADay * 5);
+        const currentTimestamp = BigNumber.from(Math.floor(Date.now() / 1000));
+
+        if (maturity.gt(currentTimestamp)) {
+          const differenceInSeconds = currentTimestamp.sub(maturity);
+
+          if (rangeInSeconds.gt(differenceInSeconds)) {
+            setDate(parseTimestamp(maturity.toString(), 'MMM DD, YYYY, HH:mm:ss'));
+            setOpenReminder(true);
+          }
+        }
+      });
+    });
+  }, [accountData]);
+
+  const handleClose = useCallback(() => {
+    setOpenReminder(false);
+  }, []);
+
+  function SlideTransition(props: SlideProps) {
+    return <Slide {...props} direction="down" />;
+  }
+
+  return (
+    <Snackbar
+      open={openReminder}
+      onClose={handleClose}
+      TransitionComponent={SlideTransition}
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+    >
+      <Alert severity="warning">
+        {t('Make sure to repay your borrows before {{date}} to avoid penalty fees.', { date: date })}
+      </Alert>
+    </Snackbar>
+  );
+}
