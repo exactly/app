4d32e452561985212a7f7931fce4b365f57d95bf
Jorge Galat <jrgglt@gmail.com>
2023-04-28 16:30:04 -0300

    ✅ fix e2e

--- hooks/useEstimateGas.ts
+++ hooks/useEstimateGas.ts
@@ -1,12 +1,18 @@
 import { useCallback, useMemo } from 'react';
 import { AlchemyProvider } from '@ethersproject/providers';
 import { PopulatedTransaction } from '@ethersproject/contracts';
+import { useProvider } from 'wagmi';
 
 import { useWeb3 } from './useWeb3';
+import { isE2E } from 'utils/client';
 
 export default function useEstimateGas() {
   const { chain } = useWeb3();
-  const provider = useMemo(() => new AlchemyProvider(chain.id, process.env.NEXT_PUBLIC_ALCHEMY_API_KEY), [chain.id]);
+  const e2eProvider = useProvider();
+  const provider = useMemo(
+    () => (isE2E ? e2eProvider : new AlchemyProvider(chain.id, process.env.NEXT_PUBLIC_ALCHEMY_API_KEY)),
+    [chain.id, e2eProvider],
+  );
 
   return useCallback(
     async (tx: PopulatedTransaction) => {
