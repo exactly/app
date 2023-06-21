2d2ff6bf9e21d5d6fb0d5324a092a87b2937ede7
Jorge Galat <jrgglt@gmail.com>
2023-06-21 13:05:18 -0300

    🩹 permit: add domain type

--- components/DebtManager/Operation/types.ts
+++
@@ -1,7 +0,0 @@
-export const Permit = [
-  { name: 'owner', type: 'address' },
-  { name: 'spender', type: 'address' },
-  { name: 'value', type: 'uint256' },
-  { name: 'nonce', type: 'uint256' },
-  { name: 'deadline', type: 'uint256' },
-] as const;
