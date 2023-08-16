2a3d7d00fa2e1b284ba47257a0ce5f0ad054f9bd
Juan Pablo Rombola <juampi.rombola@gmail.com>
2023-08-16 16:31:09 -0300

    ♻️ dashboard: refactor fixed deposit apr

--- utils/calculateAPR.ts
+++ utils/calculateAPR.ts
@@ -3,7 +3,5 @@ import { WEI_PER_ETHER } from 'utils/const';
 const ONE_YEAR = 31_536_000n;
 
 export function calculateAPR(fee: bigint, assets: bigint, timestamp: bigint, maturity: bigint): bigint {
-  const transactionRate = (fee * WEI_PER_ETHER) / assets;
-  const time = ONE_YEAR / (maturity - timestamp);
-  return transactionRate * time * 100n;
+  return ((fee * WEI_PER_ETHER * ONE_YEAR) / (assets * (maturity - timestamp))) * 100n;
 }
