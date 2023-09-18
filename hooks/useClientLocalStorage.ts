5fad3a160c9e66e251ac3e39ab6b9ab8ae67a606
Juan Pablo Rombola <juampi.rombola@gmail.com>
2023-09-18 09:15:25 -0300

    ♻️ refactor useClientLocalStorage to support number and boolean

--- hooks/useClientLocalStorage.ts
+++ hooks/useClientLocalStorage.ts
@@ -4,12 +4,41 @@ export default function useClientLocalStorage<T extends number | string | boolea
   const [value, setValue] = useState<T | undefined>();
 
   useEffect(() => {
-    setValue((localStorage.getItem(key) as T) || def);
+    const storedValue = localStorage.getItem(key);
+    if (storedValue === null && def !== undefined) {
+      setValue(def);
+    } else if (storedValue !== null) {
+      switch (typeof def) {
+        case 'number':
+          setValue(parseFloat(storedValue) as T);
+          break;
+        case 'boolean':
+          setValue((storedValue === 'true') as T);
+          break;
+        default:
+          setValue(storedValue as T);
+          break;
+      }
+    }
   }, [def, key]);
 
   const _setValue = useCallback(
     (_value: T) => {
-      localStorage.setItem(key, _value as unknown as string);
+      let storageValue: string;
+
+      switch (typeof _value) {
+        case 'number':
+          storageValue = String(_value);
+          break;
+        case 'boolean':
+          storageValue = _value ? 'true' : 'false';
+          break;
+        default:
+          storageValue = _value as string;
+          break;
+      }
+
+      localStorage.setItem(key, storageValue);
       setValue(_value);
     },
     [key],
