709a12a59061aa6bcd290f094a876d2d38171725
Juan Pablo Rombola <juampi.rombola@gmail.com>
2023-09-21 21:02:12 -0300

    ♻️ governance: change proposals section

--- components/governance/Proposals/index.tsx
+++ components/governance/Proposals/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Box, Divider, Typography } from '@mui/material';
+import { Box, Button, Divider, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 const Proposals = () => {
   const { t } = useTranslation();
@@ -9,23 +10,20 @@ const Proposals = () => {
     <Box display="flex" flexDirection="column" gap={4}>
       <Divider flexItem />
       <Box display="flex" flexDirection="column" gap={4}>
-        <Box display="flex" alignItems="center" gap={1}>
+        <Box display="flex" flexDirection="column" gap={3}>
           <Typography variant="h6">{t('Proposals')}</Typography>
-          <Typography
-            fontSize={12}
-            fontWeight={700}
-            color="grey.100"
-            sx={{ px: 0.5, py: 0.1, bgcolor: 'grey.900', borderRadius: '4px' }}
-            textTransform="uppercase"
-          >
-            {t('Coming soon')}
-          </Typography>
+          <Box display="flex" flexDirection="column" gap={0.5}>
+            <Typography fontSize={14}>{t('See our proposals on the governance forum via Snapshot.')}</Typography>
+            <Typography fontSize={14}>
+              {t("You can also create your own proposals and vote on other's proposals.")}
+            </Typography>
+          </Box>
         </Box>
-        <Typography fontSize={14} color="grey.500">
-          {t(
-            "Stay tuned to our Discord and Twitter for updates, and get ready to vote and shape the protocol's evolution.",
-          )}
-        </Typography>
+        <a href="https://gov.exact.ly/" target="_blank" rel="noreferrer noopener">
+          <Button variant="outlined" fullWidth endIcon={<OpenInNewIcon sx={{ height: 12 }} />}>
+            https://gov.exact.ly/
+          </Button>
+        </a>
       </Box>
     </Box>
   );
