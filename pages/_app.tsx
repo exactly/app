026794444a405837a8cfed06012a19e291642e67
sebipap <sebipaps@gmail.com>
2023-11-27 16:38:53 -0300

    🚸 favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,22 @@ import { ThemeProvider } from "@mui/material";
 
 import type { AppProps } from "next/app";
 import { darkTheme } from "../styles/theme";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Exa App</title>
+        <meta
+          name="description"
+          content="Deposit and borrow on-chain. The first self-custodial debit and credit card."
+        />
+        <link rel="icon" href="/icon.ico" />
+      </Head>
+      <ThemeProvider theme={darkTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
