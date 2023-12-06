c16a577857aa9e0de760200b43896d3f86827a83
franm <fran.marienhoff@gmail.com>
2023-12-06 13:31:24 -0300

    ✨show USDC bridged name

--- utils/formatSymbol.ts
+++ utils/formatSymbol.ts
@@ -1,1 +1,8 @@
-export default (value: string) => (value === 'WETH' ? 'ETH' : value);
+export default (value: string) => {
+  if (value === 'WETH') {
+    return 'ETH';
+  } else if (value === 'USDC') {
+    return 'USDC.e';
+  }
+  return value;
+};
