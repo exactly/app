9f51e383316984bef02ef42f6fc5ce216dbf6196
Jorge Galat <jrgglt@gmail.com>
2024-01-16 14:34:32 -0300

    ✨ irm: fixed calc

--- utils/fixedMath.ts
+++ utils/fixedMath.ts
@@ -85,3 +85,37 @@ export function bmin(...args: bigint[]): bigint {
 export function abs(x: bigint): bigint {
   return x < 0n ? -x : x;
 }
+
+export function sqrtWad(x: bigint): bigint {
+  if (x === 0n) return 0n;
+
+  let y = x;
+  let z = 181n;
+
+  if (y >= 0x10000000000000000000000000000000000n) {
+    y >>= 128n;
+    z <<= 64n;
+  }
+  if (y >= 0x1000000000000000000n) {
+    y >>= 64n;
+    z <<= 32n;
+  }
+  if (y >= 0x10000000000n) {
+    y >>= 32n;
+    z <<= 16n;
+  }
+  if (y >= 0x1000000n) {
+    y >>= 16n;
+    z <<= 8n;
+  }
+
+  z = (z * (y + 65536n)) >> 18n;
+
+  for (let i = 0; i < 7; ++i) {
+    z = (z + x / z) >> 1n;
+  }
+
+  if (x / z < z) z--;
+
+  return z;
+}
