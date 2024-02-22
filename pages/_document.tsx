54ade0cea71f86ae0f8c80550684d2b10d470891
sebipap <sebipaps@gmail.com>
2024-02-22 12:15:48 -0300

    🚸 add opengraph meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,15 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100;200;300;400;500;600;700&family=Inter:wght@100;200;300;400;500;600;700&&family=Bebas+Neue:wght@400&display=swap"
           rel="stylesheet"
         />
+        <meta property="og:title" content="Exa App" />
+        <meta
+          property="og:description"
+          content="Deposit and borrow on-chain. The first self-custodial debit and credit card."
+        />
+        <meta
+          property="og:image"
+          content="https://exactly.app/opengraph-image.png"
+        />
       </Head>
       <body>
         <Main />
