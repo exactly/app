140569d895f19f4dc1bda67b406334f5f3a55ce7
danilo neves cruz <cruzdanilo@gmail.com>
2024-03-20 13:59:29 -0300

    ⏪ assets: restore historical rate chart

--- components/asset/FloatingPool/index.tsx
+++ components/asset/FloatingPool/index.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 
 import FloatingPoolInfo from './FloatingPoolInfo';
 import { Box, Grid } from '@mui/material';
-// import HistoricalRateChart from 'components/charts/HistoricalRateChart';
+import HistoricalRateChart from 'components/charts/HistoricalRateChart';
 import UtilizationRateChart from 'components/charts/UtilizationRateChart';
 
 type AssetFloatingPoolProps = {
@@ -23,7 +23,7 @@ const AssetFloatingPool: FC<AssetFloatingPoolProps> = ({ symbol }) => {
       >
         <FloatingPoolInfo symbol={symbol} />
       </Grid>
-      {/* <Box
+      <Box
         boxShadow={({ palette }) => (palette.mode === 'light' ? '0px 4px 12px rgba(175, 177, 182, 0.2)' : '')}
         borderRadius="0px 0px 6px 6px"
         bgcolor="components.bg"
@@ -31,7 +31,7 @@ const AssetFloatingPool: FC<AssetFloatingPoolProps> = ({ symbol }) => {
         height={280}
       >
         <HistoricalRateChart symbol={symbol} />
-      </Box> */}
+      </Box>
       <Box
         boxShadow={({ palette }) => (palette.mode === 'light' ? '0px 4px 12px rgba(175, 177, 182, 0.2)' : '')}
         borderRadius="0px 0px 6px 6px"
