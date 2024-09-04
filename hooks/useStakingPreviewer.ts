4e09c9ab76de7fa0b9e155f64e16b2e811f401f1
franm <fran.marienhoff@gmail.com>
2024-09-04 11:39:08 -0300

    ⬆️staking: update protocol

--- hooks/useStakingPreviewer.ts
+++ hooks/useStakingPreviewer.ts
@@ -1,14 +1,16 @@
 import { isAddress, zeroAddress } from 'viem';
-import { optimismSepolia } from 'wagmi/chains';
+import { optimismSepolia, optimism } from 'wagmi/chains';
 
 import { useWeb3 } from './useWeb3';
 import { useStakingPreviewerAllClaimable, useStakingPreviewerStaking } from 'types/abi';
 import sepoliaPreviewer from '@exactly/protocol/deployments/op-sepolia/StakingPreviewer.json' assert { type: 'json' };
+import optimismPreviewer from '@exactly/protocol/deployments/optimism/StakingPreviewer.json' assert { type: 'json' };
 
 export const usePreviewerStaking = (override?: number) => {
   const { chain, walletAddress } = useWeb3();
 
   const address = {
+    [optimism.id]: optimismPreviewer.address,
     [optimismSepolia.id]: sepoliaPreviewer.address,
   }[override ?? chain.id];
 
